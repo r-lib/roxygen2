dc3bf30145e5b2d0eda2443e293abd90cdbcbe40
Peter Danenberg <pcd@roxygen.org>
2008-06-02 18:03:15 +0000

    clarify example
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/roxygen/pkg@18 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/example.R
+++ R/example.R
@@ -4,23 +4,23 @@
 #' @slot birthyear The year of birth
 #' @prototype Prototype person is named John Doe
 #'      and born in the year 1971
-setClass('Person',
-         representation = representation (
-                     fullname = 'character',
-                     birthyear = 'numeric') ,
-         prototype = prototype (
-                     fullname = 'John Doe',
-                     birthyear = '1971' ))
+setClass('person',
+         representation=
+         representation(fullname='character',
+                        birthyear='numeric'),
+         prototype=
+         prototype(fullname='John Doe',
+                   birthyear=1971))
+
 #' The naming of an object.
 #'
 #' @param object A object which gets a name
-setGeneric('name', function(object, ...){})
+setGeneric('name', function(object) standardGeneric('name'))
+
 #' Name a person, the baptism.
 #'
 #' @param object A Person object
-#' @param ... Not used
 #' @export
-setMethod('name', signature('Person'),
-function(object, ...) {
-  ...
-})
+setMethod('name', 'person', function(object) object@fullname)
+
+## name(new('person', fullname='Jane Roe', birthyear=1776))

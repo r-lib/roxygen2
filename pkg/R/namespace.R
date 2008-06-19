ec0f4adb06b94a8aa064bb4d53ad4779b1977776
pcd <pcd@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-06-19 07:12:16 +0000

    namespace roclet POC
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/roxygen@46 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/namespace.R
@@ -0,0 +1,44 @@
+namespace <- function(partita) {
+  parse.default <- function(procedure, parameters)
+    sprintf('%s(%s)', procedure, gsub(' {2,}', ', ', parameters))
+  
+  parse.exportClass <- function(procedure, parameters)
+    parse.default('exportClasses', parameters)
+
+  parse.exportMethod <- function(procedure, parameters)
+    parse.default('exportMethods', parameters)
+
+  parsers <- list(exportClass=parse.exportClass,
+                  exportMethod=parse.exportMethod)
+
+  relevators <- c('export',
+                  'exportClass',
+                  'exportMethod',
+                  'exportPattern',
+                  'S3method',
+                  'import',
+                  'importFrom',
+                  'importClassesFrom',
+                  'importMethodsFrom')
+  relevata <-
+    Reduce(append,
+           Map(function(partitum)
+               Filter(function(key.value) car(key.value) %in% relevators,
+                      zip.list(attributes(partitum)$names,
+                               partitum)),
+               partita),
+           NULL)
+  directives <-
+    Map(function(relevatum) {
+      procedure <- car(relevatum)
+      parameters <- cadr(relevatum)
+      parser <- parsers[[procedure]]
+      if (is.null(parser))
+        parse.default(procedure, parameters)
+      else
+        parser(procedure, parameters)
+    },
+        relevata)
+  for (directive in directives)
+    cat(directive, "\n")
+}

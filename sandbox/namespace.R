24a728b5a8631fa5e69c5fb175349f54a50ae855
pcd <pcd@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-06-19 07:12:16 +0000

    namespace roclet POC
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/roxygen/pkg@46 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ sandbox/namespace.R
@@ -0,0 +1,14 @@
+source('../R/functional.R')
+source('../R/list.R')
+source('../R/parse.R')
+source('../R/namespace.R')
+
+FILES <- list('example-function-mcpi.R',
+              'example-S3-mcpi.R',
+              'example-S4-person.R')
+
+argv <- commandArgs(trailingOnly=T)
+argc <- length(argv)
+files <- if (argc > 0) as.list(argv) else FILES
+
+namespace(do.call(parse.files, files))

c5f27f9a8422ff1364ca6ab36778fcead9f747a7
pcd <pcd@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-07-19 21:04:34 +0000

    preliminary Rd; sloppy parsing
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/roxygen/pkg@49 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ sandbox/Rd.R
@@ -0,0 +1,12 @@
+source('../R/functional.R')
+source('../R/list.R')
+source('../R/parse.R')
+source('../R/Rd.R')
+
+FILE <- 'example-Rd-nlm.R'
+
+argv <- commandArgs(trailingOnly=T)
+argc <- length(argv)
+file <- ifelse(argc > 0, car(argv), FILE)
+
+Rd(parse.file(file))

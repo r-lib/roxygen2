3255d19103a7725588ff59580756c8d988f2cbb1
Peter Danenberg <pcd@roxygen.org>
2008-07-20 05:08:11 +0000

    general roclet in closure style
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/roxygen/pkg@61 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/namespace.R
+++ R/namespace.R
@@ -1,23 +1,25 @@
 #' @include roclet.R
 #' @include string.R
-parse.namespace.directive <- function(proc, parms)
-  cat(sprintf('%s(%s)\n', proc, strmap(Identity, ', ', parms)))
+make.namespace.roclet <- function() {
+  parse.directive <- function(proc, parms)
+    cat(sprintf('%s(%s)\n', proc, strmap(Identity, ', ', parms)))
   
-namespace.roclet <- roclet(parse.namespace.directive)
+  roclet <- roclet(parse.directive)
 
-namespace.roclet$register.parser('exportClass',
-                                 function(proc, parms)
-                                 default.parse('exportClasses', parms))
-namespace.roclet$register.parser('exportMethod',
-                                 function(proc, parms)
-                                 default.parse('exportMethods', parms))
+  roclet$register.parser('exportClass',
+                                   function(proc, parms)
+                                   default.parse('exportClasses', parms))
+  roclet$register.parser('exportMethod',
+                                   function(proc, parms)
+                                   default.parse('exportMethods', parms))
 
-namespace.roclet$register.default.parser('export')
-namespace.roclet$register.default.parser('exportPattern')
-namespace.roclet$register.default.parser('S3method')
-namespace.roclet$register.default.parser('import')
-namespace.roclet$register.default.parser('importFrom')
-namespace.roclet$register.default.parser('importClassesFrom')
-namespace.roclet$register.default.parser('importMethodsFrom')
+  roclet$register.default.parser('export')
+  roclet$register.default.parser('exportPattern')
+  roclet$register.default.parser('S3method')
+  roclet$register.default.parser('import')
+  roclet$register.default.parser('importFrom')
+  roclet$register.default.parser('importClassesFrom')
+  roclet$register.default.parser('importMethodsFrom')
 
-namespace.roclet <- namespace.roclet$parse
+  roclet
+}

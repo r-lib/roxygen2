d4fe0516024190d565aa5762f7eaa91f8ad4f2cd
Peter Danenberg <pcd@roxygen.org>
2008-07-20 05:08:22 +0000

    Rd roclet in general style with closure
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/roxygen/pkg@62 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/roclet.R
+++ R/roclet.R
@@ -1,5 +1,7 @@
 #' @include list.R
-roclet <- function(parse.default) {
+make.roclet <- function(parse.default,
+                        pre.parse=NULL,
+                        post.parse=NULL) {
   roclet <- new.env(parent=emptyenv())
 
   roclet$parsers <- list()
@@ -7,8 +9,16 @@ roclet <- function(parse.default) {
   roclet$register.parser <- function(key, parser)
     roclet$parsers[[key]] <- parser
 
+  roclet$register.parsers <- function(parser, ...)
+    for (key in c(...))
+      roclet$register.parser(key, parser)
+
   roclet$register.default.parser <- function(key)
     roclet$parsers[[key]] <- parse.default
+
+  roclet$register.default.parsers <- function(...)
+    for (parser in c(...))
+      roclet$register.default.parser(parser)
 
   roclet$parse <- function(...) {
     key.values <- function(partitum)
@@ -19,12 +29,22 @@ roclet <- function(parse.default) {
     parser <- function(key)
       if (is.null(f <- roclet$parsers[[key]])) parse.noop else f
 
-    for (partitum in parse.files(...))
+    maybe.call <- function(proc, ...)
+      if (!is.null(proc))
+        do.call(proc, list(...))
+
+    for (partitum in parse.files(...)) {
+      maybe.call(pre.parse, partitum)
       for (key.value in key.values(partitum)) {
         key <- car(key.value)
         do.call(parser(key), list(key, cdr(key.value)))
       }
+      maybe.call(post.parse, partitum)
+    }
   }
 
   roclet
 }
+
+assign.parent <- function(var, value, env)
+  assign(var, value, envir=parent.env(env))

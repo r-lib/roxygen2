5ff4a2d81becbb9805e6cd1c74986300171c9232
Peter Danenberg <pcd@roxygen.org>
2008-07-21 02:35:33 +0000

    fix abweichende roxygen-Rd relationships; tests
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/roxygen/pkg@69 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ tests/Rd.R
@@ -0,0 +1,19 @@
+#' description
+#'
+#' details
+#' @name test
+#' @title test
+#' @usage test
+#' @return test
+#' @references test
+#' @note test
+#' @author test@@example.com
+#' @seealso test
+#' @examples test
+#' @concept test
+#' @keywords test1 test2
+#' @aliases test1 test2
+#' @param p1 first param
+#' @param p2 second param
+#' @param p3 third param
+roxygen()
---
+++ tests/namespace.R
@@ -0,0 +1,10 @@
+#' @exportClass test
+#' @exportMethod  test
+#' @export test
+#' @exportPattern test
+#' @S3method test
+#' @import test
+#' @importFrom test
+#' @importClassesFrom test
+#' @importMethodsFrom test
+roxygen()

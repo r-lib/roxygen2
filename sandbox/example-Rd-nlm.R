d23f0e52cc062b3dac51a8b96e5d8dd299ac0e05
Peter Danenberg <pcd@roxygen.org>
2008-07-24 18:16:01 +0000

    example based on file; honest strwrap in usage
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/roxygen/pkg@82 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- sandbox/example-Rd-nlm.R
+++ sandbox/example-Rd-nlm.R
@@ -105,26 +105,7 @@
 #'   \code{\link{deriv}} to calculate analytical derivatives.
 #' 
 #'   For nonlinear regression, \code{\link{nls}} may be better.
-#'
-#' @examples
-#' f <- function(x) sum((x-1:length(x))^2)
-#' nlm(f, c(10,10))
-#' nlm(f, c(10,10), print.level = 2)
-#' utils::str(nlm(f, c(5), hessian = TRUE))
-#' 
-#' f <- function(x, a) sum((x-a)^2)
-#' nlm(f, c(10,10), a=c(3,5))
-#' f <- function(x, a)
-#' {
-#'     res <- sum((x-a)^2)
-#'     attr(res, "gradient") <- 2*(x-a)
-#'     res
-#' }
-#' nlm(f, c(10,10), a=c(3,5))
-#' 
-#' ## more examples, including the use of derivatives.
-#' \dontrun{demo(nlm)}
-#'
+#' @example example-Rd-nlm-test.R
 #' @keywords nonlinear optimize
 nlm <- function(f, p, ..., hessian=FALSE, typsize=rep(1,length(p)),
 		fscale=1, print.level=0, ndigit=12, gradtol=1e-6,

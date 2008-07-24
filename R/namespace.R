f5cd4a9521077c35222313625645771c29a58405
Peter Danenberg <pcd@roxygen.org>
2008-07-24 04:36:41 +0000

    preliminary guess for empty @export
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/roxygen/pkg@80 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/namespace.R
+++ R/namespace.R
@@ -14,17 +14,52 @@ make.namespace.roclet <- function() {
   parse.directive <- function(proc, parms)
     cat(sprintf('%s(%s)\n', proc, strmap(Identity, ', ', parms)))
   
-  roclet <- make.roclet(parse.directive)
+  exportee <- NULL
 
-  roclet$register.parser('exportClass',
-                         function(proc, parms)
-                         parse.directive('exportClasses', parms))
-  roclet$register.parser('exportMethod',
-                         function(proc, parms)
-                         parse.directive('exportMethods', parms))
+  pre.parse <- function(partitum)
+    exportee <<- list(name=partitum$name,
+                      assignee=partitum$assignee,
+                      S4method=partitum$S4method,
+                      S4generic=partitum$S4method,
+                      S4class=partitum$S4class)
 
-  roclet$register.default.parsers('export',
-                                  'exportPattern',
+  roclet <- make.roclet(parse.directive,
+                        pre.parse)
+
+  parse.exportClass <- function(proc, parms)
+    parse.directive('exportClasses', parms)
+
+  roclet$register.parser('exportClass', parse.exportClass)
+
+  parse.exportMethod <- function(proc, parms)
+    parse.directive('exportMethods', parms)
+
+  roclet$register.parser('exportMethod', parse.exportMethod)
+
+  parse.export <- function(proc, parms) {
+    if (is.null.string(parms)) {
+      if (!is.null(exportee$S4method))
+        parse.exportMethod(exportee$S4method)
+      else if (!is.null(exportee$S4class))
+        parse.exportClass(exportee$S4class)
+      else if (!is.null(exportee$S4method))
+        parse.exportMethod(exportee$S4method)
+      else {
+        exportee <- first.non.null(exportee$name,
+                                   exportee$assignee)
+        if (is.null(exportee))
+          warning('Empty export directive')
+        else
+          parse.directive('export', exportee)
+      }
+    }
+  }
+
+  roclet$register
+
+  roclet$register.parser('export', parse.export)
+
+  roclet$register.default.parsers('exportPattern',
                                   'S3method',
                                   'import',
                                   'importFrom',

c25325fa14b2f0c05a8f40a887a319f4a30bcc31
pcd <pcd@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-07-25 05:11:20 +0000

    quote collation items in case of spaces in filenames
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/roxygen/pkg@89 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tests/runit/runit.collate.R
+++ tests/runit/runit.collate.R
@@ -9,9 +9,9 @@ test.collate <- function() {
                                           'runit/collate/tie.R',
                                           'runit/collate/undershorts.R',
                                           'runit/collate/watch.R')),
-              paste('Collate: runit/collate/undershorts.R',
-                    'runit/collate/pants.R runit/collate/belt.R',
-                    'runit/collate/shirt.R runit/collate/tie.R',
-                    'runit/collate/jacket.R runit/collate/socks.R',
-                    'runit/collate/shoes.R runit/collate/watch.R'))
+              paste("Collate: 'runit/collate/undershorts.R'",
+                    "'runit/collate/pants.R' 'runit/collate/belt.R'",
+                    "'runit/collate/shirt.R' 'runit/collate/tie.R'",
+                    "'runit/collate/jacket.R' 'runit/collate/socks.R'",
+                    "'runit/collate/shoes.R' 'runit/collate/watch.R'"))
 }

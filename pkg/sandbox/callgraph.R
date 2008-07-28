83020c3e08bf785275a80d725a979032c2ff7364
pcd <pcd@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-07-28 15:09:53 +0000

    one level
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/roxygen@122 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/sandbox/callgraph.R
+++ pkg/sandbox/callgraph.R
@@ -48,7 +48,7 @@ is.callable <- function(name) {
 
 exprofundum <- expression(roxygenize)
 
-discover.subcalls <- function(exprofundum, depth=2)
+discover.subcalls <- function(exprofundum, depth=1)
   if (is.name(exprofundum)) {
     subcall <- as.character(exprofundum)
     if (is.callable(subcall) && call.stack$top <= depth) {

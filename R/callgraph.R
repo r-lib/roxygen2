942d30019b5c6606a65e8dd18c4515a9348a9b0f
pcd <pcd@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-07-29 04:26:58 +0000

    callgraph stub
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/roxygen/pkg@129 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/callgraph.R
@@ -0,0 +1,8 @@
+#' @include roxygen.R
+#' @include roclet.R
+roclet()
+
+make.callgraph.roclet <- function(outfile='',
+                                  verbose=TRUE) {
+  ## Need to abstract some parse.name stuff out of Rd
+}

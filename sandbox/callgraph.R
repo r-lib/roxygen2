61170266824c28b61cb5f5c3f67fe958f7c1c012
Peter Danenberg <pcd@roxygen.org>
2008-07-29 04:19:18 +0000

    various ridiculous graphs
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/roxygen/pkg@128 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- sandbox/callgraph.R
+++ sandbox/callgraph.R
@@ -53,7 +53,7 @@ exprofundum <- expression(append)
 exprofundum <- expression(roxygenize)
 
 discover.subcalls <- function(exprofundum,
-                              depth=2,
+                              depth=6,
                               include.primitives=FALSE)
   if (is.name(exprofundum)) {
     subcall <- as.character(exprofundum)

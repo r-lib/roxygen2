ca7fb3084b2cbeeb7023bf2dece5c8afbca85234
pcd <pcd@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-08-24 08:37:19 +0000

    scrunch line-width for LaTeX listing; inchoate vignette
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/roxygen/pkg@179 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- sandbox/example-pseudoprime.R
+++ sandbox/example-pseudoprime.R
@@ -1,23 +1,26 @@
 #' Test an integer for primality with Fermat's Little Theorem.
 #'
-#' Fermat's Little Theorem states that if \eqn{n} is a prime number and
-#' \eqn{a} is any positive integer less than \eqn{n}, then \eqn{a} raised
-#' to the \eqn{n}th power is congruent to \eqn{a modulo n}.
+#' Fermat's Little Theorem states that if \eqn{n} is a prime
+#' number and \eqn{a} is any positive integer less than \eqn{n},
+#' then \eqn{a} raised to the \eqn{n}th power is congruent to
+#' \eqn{a modulo n}.
 #'
 #' @author Peter Danenberg \email{pcd@@roxygen.org}
 #' @param n the integer to test for primality
 #' @return Whether the integer passes the Fermat test
-#'   for a randomized 0 < a < n
+#'   for a randomized \eqn{0 < a < n}
 fermat.test <- function(n) {
   a <- floor(runif(1, min=1, max=n))
-  isTRUE(a ^ n %% n == a)
+  a ^ n %% n == a
 }
 
-#' Check an integer for pseudo-primality to an arbitrary precision.
+#' Check an integer for pseudo-primality to an arbitrary
+#' precision.
 #'
-#' A number is pseudo-prime if it is probably prime, the basis of
-#' which is the probabilistic Fermat test; if it passes two such
-#' tests, the chances are better than 3 out of 4 that \eqn{n} is prime.
+#' A number is pseudo-prime if it is probably prime, the basis
+#' of which is the probabilistic Fermat test; if it passes two
+#' such tests, the chances are better than 3 out of 4 that
+#' \eqn{n} is prime.
 #'
 #' @author Peter Danenberg \email{pcd@@roxygen.org}
 #' @param n the integer to test for pseudoprimality.
@@ -26,12 +29,11 @@ fermat.test <- function(n) {
 #' @export
 #' @seealso \code{\link{fermat.test}}
 #' @references Abelson, Hal; Jerry Sussman, and Julie Sussman.
-#'   Structure and Interpretation of Computer Programs. Cambridge:
-#'   MIT Press, 1984.
+#'   Structure and Interpretation of Computer Programs.
+#'   Cambridge: MIT Press, 1984.
 #' @keywords pseudoprime fermat
 #' @examples
-#'   ## Will be true most of the time
-#'   is.pseudoprime(13, 4)
+#' is.pseudoprime(13, 4)  # TRUE most of the time
 is.pseudoprime <- function(n, times) {
   if (times == 0) TRUE
   else if (fermat.test(n)) is.fermat.prime(n, times - 1)

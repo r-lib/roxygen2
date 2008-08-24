6cc0812251d9d4d143241fa2d28f7b23ba8491a1
Peter Danenberg <pcd@roxygen.org>
2008-08-24 08:37:14 +0000

    less verbose; exdent
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/roxygen/pkg@177 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- sandbox/example-pseudoprime.R
+++ sandbox/example-pseudoprime.R
@@ -4,14 +4,10 @@
 #' \eqn{a} is any positive integer less than \eqn{n}, then a raised to the
 #' \eqn{n}th power is congruent to \eqn{a modulo n}.
 #'
+#' @author Peter Danenberg \email{pcd@@roxygen.org}
 #' @param n the integer to test for primality
 #' @return Whether the integer passes the Fermat test
-#' for a randomized 0 < a < n
-#' @note Can't test for primes above 15 because of loss of precision
-#' in modulus.
-#' @TODO Use the \code{gmp} library so we can test for bigger
-#' primes.
-#' @author Peter Danenberg \email{pcd@@roxygen.org}
+#'   for a randomized 0 < a < n
 fermat.test <- function(n) {
   a <- floor(runif(1, min=1, max=n))
   isTRUE(a ^ n %% n == a)
@@ -21,21 +17,21 @@ fermat.test <- function(n) {
 #'
 #' A number is pseudo-prime if it is probably prime, the basis of
 #' which is the probabilistic Fermat test; if it passes two such
-#' tests, the chances are better than 3 out of 4 that n is prime.
+#' tests, the chances are better than 3 out of 4 that \eqn{n} is prime.
 #'
+#' @author Peter Danenberg \email{pcd@@roxygen.org}
 #' @param n the integer to test for pseudoprimality.
 #' @param times the number of Fermat tests to perform
 #' @return Whether the number is pseudoprime
-#' @seealso \code{\link{fermat.test}}
 #' @export
-#' @examples
-#' ## Will be true most of the time
-#' is.pseudoprime(13, 4)
+#' @seealso \code{\link{fermat.test}}
 #' @references Abelson, Hal; Jerry Sussman, and Julie Sussman.
-#' Structure and Interpretation of Computer Programs. Cambridge:
-#' MIT Press, 1984.
+#'   Structure and Interpretation of Computer Programs. Cambridge:
+#'   MIT Press, 1984.
 #' @keywords pseudoprime fermat
-#' @author Peter Danenberg \email{pcd@@roxygen.org}
+#' @examples
+#'   ## Will be true most of the time
+#'   is.pseudoprime(13, 4)
 is.pseudoprime <- function(n, times) {
   if (times == 0) TRUE
   else if (fermat.test(n)) is.fermat.prime(n, times - 1)

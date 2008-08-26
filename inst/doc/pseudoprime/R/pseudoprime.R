972c43a2216429f1b83ee95aac77ee8e6791b41d
Peter Danenberg <pcd@roxygen.org>
2008-08-26 18:20:24 +0000

    expound upon fermat test; break up pseudoprime.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/roxygen/pkg@191 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- inst/doc/pseudoprime/R/pseudoprime.R
+++ inst/doc/pseudoprime/R/pseudoprime.R
@@ -1,21 +1,5 @@
-#' Test an integer for primality with Fermat's Little Theorem.
-#'
-#' Fermat's Little Theorem states that if \eqn{n} is a prime
-#' number and \eqn{a} is any positive integer less than \eqn{n},
-#' then \eqn{a} raised to the \eqn{n}th power is congruent to
-#' \eqn{a\ modulo\ n}{a modulo n}.
-#'
-#' @param n the integer to test for primality
-#' @return Whether the integer passes the Fermat test
-#'   for a randomized \eqn{0 < a < n}
-#' @callGraphPrimitives
-#' @note \code{fermat.test} doesn't work for integers above
-#'   approximately fifteen because modulus loses precision.
-#' @author Peter Danenberg \email{pcd@@roxygen.org}
-fermat.test <- function(n) {
-  a <- floor(runif(1, min=1, max=n))
-  a ^ n %% n == a
-}
+#' @include fermat.R
+roxygen()
 
 #' Check an integer for pseudo-primality to an arbitrary
 #' precision.

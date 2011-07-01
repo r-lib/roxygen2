42ee037123f3573d2f0622b21aa6b6f7dd5a42fd
hadley <h.wickham@gmail.com>
2011-06-30 19:21:11 -0500

    Remove redundant copy.rd docs

--- R/roxygenize.R
+++ R/roxygenize.R
@@ -1,50 +1,14 @@
-#' Recursively copy a directory thither; optionally unlinking
-#' the target first; optionally overwriting; optionally
-#' verbalizing.
-#' @param source the source directory
-#' @param target the target directory
-#' @param unlink.target delete target directory first?
-#' @param overwrite overwrite target files?
-#' @param verbose verbalize transaction?
-#' @return \code{NULL}
-#' @note Not tested on non-linux platforms
-copy.dir <- function(source,
-                     target = source,
-                     unlink.target=FALSE,
-                     overwrite=FALSE,
-                     verbose=FALSE) {
-  if (unlink.target)
-    unlink(target, recursive=TRUE)
-  files <- list.files(source,
-                      full.name=TRUE,
-                      recursive=TRUE,
-                      all.files=TRUE)
-  for (source.file in files) {
-    promoted.file <- sub('[^/\\]*(/|\\\\)', '', source.file)
-    target.file <- file.path(target, promoted.file)
-    target.dir <- dirname(target.file)
-    ## Could create, instead, a list of unique directories in
-    ## Theta(n).
-    dir.create(target.dir, recursive=TRUE, showWarnings=FALSE)
-    if (verbose)
-      cat(sprintf('%s -> %s', source.file, target.file), '\n')
-    file.copy(source.file, target.file, overwrite=overwrite)
-  }
-}
-
 #' Process a package with the Rd, namespace and collate roclets.
+#'
 #' @param package.dir the package's top directory
 #' @param roxygen.dir where to create roxygen output; defaults to
-#' \file{package.roxygen}.
-#' @param copy.package copies the package over before
-#' adding/manipulating files.
-#' @param overwrite overwrite target files
-#' @param unlink.target unlink target directory before
-#' processing files
-#' @param use.Rd2 use the Rd2 roclet
+#'   \file{package.roxygen}.
+#' @param copy.package copies the package over before adding/manipulating
+#'    files.
+#' @param overwrite overwrite target files?
+#' @param unlink.target unlink target directory before processing files?
+#' @param roclets character vector of roclet names to apply to package
 #' @return \code{NULL}
-#' @TODO Options to enable/disable specific roclet
-#' (\command{--no-callgraphs}, etc.)
 #' @export roxygenize roxygenise
 roxygenize <- function(package.dir,
                        roxygen.dir=package.dir,
@@ -72,3 +36,32 @@ roxygenize <- function(package.dir,
 }
 
 roxygenise <- roxygenize
+
+# Recursively copy a directory thither; optionally unlinking
+# the target first; optionally overwriting; optionally
+# verbalizing.
+#
+# @note Not tested on non-linux platforms
+copy.dir <- function(source,
+                     target = source,
+                     unlink.target=FALSE,
+                     overwrite=FALSE,
+                     verbose=FALSE) {
+  if (unlink.target)
+    unlink(target, recursive=TRUE)
+  files <- list.files(source,
+                      full.name=TRUE,
+                      recursive=TRUE,
+                      all.files=TRUE)
+  for (source.file in files) {
+    promoted.file <- sub('[^/\\]*(/|\\\\)', '', source.file)
+    target.file <- file.path(target, promoted.file)
+    target.dir <- dirname(target.file)
+    ## Could create, instead, a list of unique directories in
+    ## Theta(n).
+    dir.create(target.dir, recursive=TRUE, showWarnings=FALSE)
+    if (verbose)
+      cat(sprintf('%s -> %s', source.file, target.file), '\n')
+    file.copy(source.file, target.file, overwrite=overwrite)
+  }
+}

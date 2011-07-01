c85df2594f9e6fe5411897b6642d4e15e87ebe7a
hadley <h.wickham@gmail.com>
2011-07-01 16:41:31 -0500

    Tidy up utils

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,25 @@
 "%||%" <- function(a, b) {
   if (!is.null(a)) a else b
 }
+
+# Given argument list, produce usage string for it.
+# 
+# Adapted from \code{\link{prompt}}.
+#
+# @param f function, or name of function, as string
+# @return a string
+usage <- function(args) {
+  is.missing.arg <- function(arg) {
+    is.symbol(arg) && deparse(arg) == ""
+  }
+  arg_to_text <- function(arg) {
+    if (is.missing.arg(arg)) return("")
+    text <- deparse(arg, backtick = TRUE, width.cutoff = 500L)
+    
+    paste(" = ", paste(text, collapse = "\n"), sep = "")
+  }
+
+  arg_values <- vapply(args, arg_to_text, character(1))
+  
+  paste(names(args), arg_values, collapse = ", ", sep = "")
+}

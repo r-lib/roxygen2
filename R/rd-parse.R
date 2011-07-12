751353085e8827d6aed9df340837380d682b3d88
hadley <h.wickham@gmail.com>
2011-07-12 16:33:37 -0500

    Add rd-parse to collate. Tweak parsing

--- R/rd-parse.R
+++ R/rd-parse.R
@@ -4,9 +4,10 @@ get_rd <- function(topic, package = NULL) {
   utils:::.getHelpFile(top)
 }
 
-rd_tag <- function(x) attr(x, "Rd_tag")
 
-has_tag <- function(rd, tag) {
+get_tags <- function(rd, tag) {
+  rd_tag <- function(x) attr(x, "Rd_tag")
+
   Filter(function(x) rd_tag(x) == tag, rd)
 }
 
@@ -16,8 +17,8 @@ rd2rd <- function(x) {
 
 # rd_arguments(get_rd("mean"))
 rd_arguments <- function(rd) {
-  arguments <- has_tag(rd, "\\arguments")[[1]]
-  items <- has_tag(arguments, "\\item")
+  arguments <- get_tags(rd, "\\arguments")[[1]]
+  items <- get_tags(arguments, "\\item")
   
   values <- lapply(items, function(x) rd2rd(x[[2]]))
   params <- vapply(items, function(x) rd2rd(x[[1]]), character(1))

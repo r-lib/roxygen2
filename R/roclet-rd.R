cd6b6d6129f1c6caf0427a689616416ad7a23e04
hadley <h.wickham@gmail.com>
2011-07-13 09:22:08 -0500

    Skip seealso for single member families

--- R/roclet-rd.R
+++ R/roclet-rd.R
@@ -240,6 +240,9 @@ roc_process.had <- function(roclet, partita, base_path) {
     for(topic_name in related) {
       topic <- topics[[topic_name]]
       others <- setdiff(related, topic_name)
+      
+      if (length(others) < 1) next;
+      
       other_topics <- sort(unlist(name_lookup[others], use.names = FALSE))
 
       links <- paste("\\code{\\link{", other_topics, "}}", 

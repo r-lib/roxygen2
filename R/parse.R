5619c43ae29487ee1d575892132b45dedf6356fb
hadley <h.wickham@gmail.com>
2011-11-06 17:11:28 +1300

    Use modifyList, not c, to combine prerefs and srcrefs

--- R/parse.R
+++ R/parse.R
@@ -18,7 +18,7 @@ parse.file <- function(file, env, env_hash) {
 
     stopifnot(length(src_parsed) == length(pre_parsed))
 
-    mapply(c, src_parsed, pre_parsed, SIMPLIFY = FALSE)    
+    mapply(modifyList, src_parsed, pre_parsed, SIMPLIFY = FALSE)
   })
 }
 

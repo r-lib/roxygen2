r-lib/roxygen2

commit a0188a263df1a2c409f844e6f3e5867ef799d104

* Fix issue with aliases in S4 classes: it can -- often -- happen that a function or generic exists with the same name as the class (e.g. a constructor method), which causes problems to the lookup of inherited parameters, which finds multiple Rd files associated with the topic. This commit ensure that the plain name of the class is added to the alias list only if not function exists with the same name (at least within in the package). Also default rdname (src_name) should be NAME-class, consistently with what package.skeleton produces.

R/srcrefs.R
@@ -29,11 +29,24 @@ parse_class <- function(call, env) {
   name <- as.character(call$Class)
   class <- getClass(name, where = env)
 
+  # Default alias is A-class
+  aliases <- str_c(name, "-class")
+  # add plain name to aliases only if there is no function named like the class, 
+  # which would typically be a constructor method. 
+  if( is.null(getFunction(name, where = env, mustFind=FALSE)) ){
+	  aliases <- c(name, aliases)
+  }
+  
   # class?classRepresentation
+  # - Default rdname should be A-class.rd and not duplicate alias if a 
+  # function exists with the same name as the class (see above). 
+  # NB: name is added to aliases in roclet_rd_one by:
+  # add_tag(rd, new_tag("alias", partitum$name %||% partitum$src_alias))
+  #
   list(
     src_type = "class",
-    src_name = name,
-    src_alias = c(name, str_c(name, "-class")),
+    src_name = str_c(name, "-class"), 
+    src_alias = aliases,
     extends = showExtends(class@contains, printTo = FALSE),
     slots = class@slots
   )

inst/tests/test-rd-s4.R
@@ -231,6 +231,34 @@ test_that("@usage for S4methods", {
     c("show,array-method"))
 })
 
+test_that("S4 classes have correct aliases", {
+	
+	# check only default alias NAME-class if a function exists with same name
+	.test <- function(chunk, aliases, ...){
+		
+		chunk <- str_c("
+		#' Important class.
+		#' 
+		setClass('AAA')
+		", chunk, "\n")
+		out <- roc_proc_text(roc, chunk)[[1]]
+		
+		expect_equal(get_tag(out, "alias")$values, aliases
+				, info="Aliases are correct", ...)
+	}
+	
+	.test(""
+		, aliases=c("AAA", "AAA-class")
+		, label="No function exists with same name")
+	.test("AAA <- function(){}"
+		, aliases="AAA-class"
+		, label="A standard function exists with same name")
+	.test("setGeneric('AAA', function(object) standardGenric('AAA'))"
+		, aliases="AAA-class"
+		, label="An S4 generic exists with same name")
+	
+})
+
 test_that("@slot creates a new section and lists slots", {
   out <- roc_proc_text(roc, "
     #' Important class.

commit 6034a9d02b3422a89c50b6edb2f7e5d3cab3ac85

Bug fixes for S4 classes:\n* fix a bug introduced with commit a0188a263df1a2c409f844e6f3e5867ef799d104: define another element  to hold data about default name, as element  is used elsewhere as the R access name for classes (error was identified running the unit tests)\n * change unit test for aliases of S4 classes, which should include the plain name even if a functions with the same name exists => will pop up R Rd selection tools.

R/roclet-rd.R
@@ -289,7 +289,7 @@ roclet_rd_one <- function(partitum, base_path) {
   if (!has_rd || dont_rd) return()
   
   # Figure out topic name
-  name <- partitum$name %||% partitum$src_name  
+  name <- partitum$name %||% partitum$src_topic %||% partitum$src_name  
   if (is.null(name)) roxygen_stop("Missing name", srcref = partitum$srcref)
 
   # Work out file name and initialise Rd object

R/srcrefs.R
@@ -29,24 +29,19 @@ parse_class <- function(call, env) {
   name <- as.character(call$Class)
   class <- getClass(name, where = env)
 
-  # Default alias is A-class
-  aliases <- str_c(name, "-class")
-  # add plain name to aliases only if there is no function named like the class, 
-  # which would typically be a constructor method. 
-  if( is.null(getFunction(name, where = env, mustFind=FALSE)) ){
-	  aliases <- c(name, aliases)
-  }
-  
   # class?classRepresentation
-  # - Default rdname should be A-class.rd and not duplicate alias if a 
-  # function exists with the same name as the class (see above). 
-  # NB: name is added to aliases in roclet_rd_one by:
-  # add_tag(rd, new_tag("alias", partitum$name %||% partitum$src_alias))
-  #
+  # Default @name should be CLASSNAME-class and not CLASSNAME, as it can clash 
+  # with a function with te same name (e.g. a constructor function), which 
+  # must have this @name.
+  # This is achieved via $src_topic which takes precedence over $src_name for default 
+  # @name ($src_name is used in several places as the R access name for the class)
+  # Access via ?CLASSNAME is ensured by appropriate default @alias.
+  topic <- topic_name(class)
   list(
     src_type = "class",
-    src_name = str_c(name, "-class"), 
-    src_alias = aliases,
+    src_name = name, 
+    src_alias = c(name, topic),
+	src_topic = topic,
     extends = showExtends(class@contains, printTo = FALSE),
     slots = class@slots
   )
@@ -78,7 +73,9 @@ parse_method <- function(call, env, replace=FALSE) {
   f <- getMethod(name, sig, where = env)
   pkg <- attr(f@generic, "package")
   if (pkg == "roxygen_test") {
-    inherit <- f@generic
+	# inherit from the generic defined within the package
+	# which is uniquely identified by its topic_name
+    inherit <- topic_name(getGeneric(f@generic, where = env))
   } else {
     inherit <- str_c(pkg, "::", f@generic)
   }
@@ -129,6 +126,9 @@ method_signature <- function(x){
 
 setGeneric("topic_name", function(x) {
   standardGeneric("topic_name")
+})
+setMethod("topic_name", signature(x = "classRepresentation"), function(x) {
+  str_c(x@className, "-class")
 })
 setMethod("topic_name", signature(x = "MethodDefinition"), function(x) {
   str_c(str_c(c(x@generic, method_signature(x)), collapse = ","), "-method")

inst/tests/test-rd-s4.R
@@ -269,10 +269,10 @@ test_that("S4 classes have correct aliases", {
 		, aliases=c("AAA", "AAA-class")
 		, label="No function exists with same name")
 	.test("AAA <- function(){}"
-		, aliases="AAA-class"
+		, aliases=c("AAA", "AAA-class")
 		, label="A standard function exists with same name")
 	.test("setGeneric('AAA', function(object) standardGenric('AAA'))"
-		, aliases="AAA-class"
+		, aliases=c("AAA", "AAA-class")
 		, label="An S4 generic exists with same name")
 	
 })
d74abd34cdbc7647aa5f0310dff7257c8b72c491
Mark Cowley <m.cowley@garvan.org.au>
2012-07-11 16:32:05 +1000

    match Hadley's style-guide

--- inst/tests/test-roxygenize-Imports.R
+++ inst/tests/test-roxygenize-Imports.R
@@ -1,9 +1,9 @@
 context("Update Imports directives within DESCRIPTION")
 
-test_that("namespace.imports works as expected", {
+test_that("namespace.file.imports works as expected", {
   nf <- "namespace-example1.txt"
   expect_true(file.exists(nf))
-  imp <- namespace.imports(nf)
+  imp <- namespace.file.imports(nf)
   exp <- c("Biobase", "affy", "devtools", "ggplot2")
   expect_equal(imp, exp)
 })
@@ -15,23 +15,23 @@ test_that("Imports updates as expected", {
   expect_true(file.exists(df))
   expect_true(file.exists(nf))
 
-  pkg <- temppackage(df, nf)
+  pkg <- temp_package(df, nf)
 
-  ns.imports <- namespace.imports(file.path(pkg, "NAMESPACE"))
+  ns.imports <- namespace.file.imports(file.path(pkg, "NAMESPACE"))
   exp <- c("Biobase", "affy", "devtools", "ggplot2")
   expect_equal(ns.imports, exp)
 
   desc <- read.description(file.path(pkg, "DESCRIPTION"))
-  desc.imports <- parse.dependencies(desc$Imports, exclude.R=TRUE)
+  desc.imports <- parse.dependencies(desc$Imports, exclude.R = TRUE)
   exp.desc.imports <- structure(c("stringr", "tools", "brew"), .Names = c("stringr (>= 0.5)", 
   "tools", "brew"))
   expect_equal(desc.imports, exp.desc.imports)
 
-  expect_message(roxygenize(pkg, roclets=character()), "Updating Imports directive in.*")
+  expect_message(roxygenize(pkg, roclet = character()), "Updating Imports directive in.*")
   
   # Imports in DESCRIPTION should have been updated
   desc <- read.description(file.path(pkg, "DESCRIPTION"))
-  desc.imports <- parse.dependencies(desc$Imports, exclude.R=TRUE)
+  desc.imports <- parse.dependencies(desc$Imports, exclude.R = TRUE)
   exp.desc.imports <- structure(c("Biobase", "affy", "devtools", "ggplot2"), .Names = c("Biobase", 
 	"affy", "devtools", "ggplot2"))
   expect_equal(desc.imports, exp.desc.imports)
@@ -43,23 +43,23 @@ test_that("Imports don't update as expected", {
   expect_true(file.exists(df))
   expect_true(file.exists(nf))
   
-  pkg <- temppackage(df, nf)
+  pkg <- temp_package(df, nf)
   
-  ns.imports <- namespace.imports(file.path(pkg, "NAMESPACE"))
+  ns.imports <- namespace.file.imports(file.path(pkg, "NAMESPACE"))
   exp.ns.imports <- c("brew", "stringr", "tools")
   expect_equal(ns.imports, exp.ns.imports)
   
   desc <- read.description(file.path(pkg, "DESCRIPTION"))
-  desc.imports <- parse.dependencies(desc$Imports, exclude.R=TRUE)
+  desc.imports <- parse.dependencies(desc$Imports, exclude.R = TRUE)
   exp.desc.imports <- structure(c("stringr", "tools", "brew"), .Names = c("stringr (>= 0.5)", 
   "tools", "brew"))
   expect_equal(desc.imports, exp.desc.imports)
 
-  roxygenize(pkg, roclets=character())
+  roxygenize(pkg, roclet = character())
   
   # Imports in DESCRIPTION should NOT have been updated
   desc <- read.description(file.path(pkg, "DESCRIPTION"))
-  desc.imports <- parse.dependencies(desc$Imports, exclude.R=TRUE)
+  desc.imports <- parse.dependencies(desc$Imports, exclude.R = TRUE)
   exp.desc.imports <- structure(c("stringr", "tools", "brew"), .Names = c("stringr (>= 0.5)", 
   "tools", "brew"))
   expect_equal(desc.imports, exp.desc.imports)
@@ -72,23 +72,23 @@ test_that("Imports deletions occur as expected", {
   expect_true(file.exists(df))
   expect_true(file.exists(nf))
   
-  pkg <- temppackage(df, nf)
+  pkg <- temp_package(df, nf)
   
-  ns.imports <- namespace.imports(file.path(pkg, "NAMESPACE"))
+  ns.imports <- namespace.file.imports(file.path(pkg, "NAMESPACE"))
   exp.ns.imports <- c("stringr", "tools")
   expect_equal(ns.imports, exp.ns.imports)
   
   desc <- read.description(file.path(pkg, "DESCRIPTION"))
-  desc.imports <- parse.dependencies(desc$Imports, exclude.R=TRUE)
+  desc.imports <- parse.dependencies(desc$Imports, exclude.R = TRUE)
   exp.desc.imports <- structure(c("stringr", "tools", "brew"), .Names = c("stringr (>= 0.5)", 
   "tools", "brew"))
   expect_equal(desc.imports, exp.desc.imports)
 
-  expect_message(roxygenize(pkg, roclets=character()), "Updating Imports directive in.*")
+  expect_message(roxygenize(pkg, roclet = character()), "Updating Imports directive in.*")
   
   # Imports in DESCRIPTION should have been updated
   desc <- read.description(file.path(pkg, "DESCRIPTION"))
-  desc.imports <- parse.dependencies(desc$Imports, exclude.R=TRUE)
+  desc.imports <- parse.dependencies(desc$Imports, exclude.R = TRUE)
   exp.desc.imports <- structure(c("stringr", "tools"), .Names = c("stringr (>= 0.5)", 
   "tools"))
   expect_equal(desc.imports, exp.desc.imports)

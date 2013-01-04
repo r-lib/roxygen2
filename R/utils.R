bab70d751fb5c7fee86db072e5006be0a8419383
Yihui Xie <xie@yihui.name>
2013-01-03 20:45:42 -0600

    need to escape \ in arguments as well

--- R/utils.R
+++ R/utils.R
@@ -15,7 +15,7 @@ usage <- function(args) {
   arg_to_text <- function(arg) {
     if (is.missing.arg(arg)) return("")
     text <- deparse(arg, backtick = TRUE, width.cutoff = 500L)
-    text <- str_replace_all(text, fixed("%"), "\\%")
+    text <- str_replace_all(text, "([%\\])", "\\\\\\1")
     text <- str_replace_all(text, fixed(" "), "\u{A0}")
     Encoding(text) <- "UTF-8"    
     

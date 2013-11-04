10078c5dcde60b538c578bd6a7b657b24274a7db
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2013-11-04 17:41:58 +0100

    template with extension .r are supported on case-sensitive file systems
    - add tests to cover existing and failing behavior

---
+++ inst/tests/test-template.R
@@ -0,0 +1,17 @@
+context("Template")
+
+test_that("template_find finds files with .r and .R extension, and fails to find missing files", {
+  my.tempdir <- "."
+  my.mandir <- file.path(my.tempdir, "man-roxygen")
+  my.ucase <- file.path(my.mandir, "UCase.R")
+  my.regex <- file.path(my.mandir, "reg.ex.R")
+  my.lcase <- file.path(my.mandir, "lcase.r")
+
+  expect_equal(template_find(my.tempdir, "UCase"), my.ucase)
+  expect_error(template_find(my.tempdir, "Case"))
+  expect_error(template_find(my.tempdir, "UCas"))
+  expect_equal(template_find(my.tempdir, "reg.ex"), my.regex)
+  expect_error(template_find(my.tempdir, "reggex"))
+  expect_error(template_find(my.tempdir, "nada"))
+  expect_equal(template_find(my.tempdir, "lcase"), my.lcase)
+})

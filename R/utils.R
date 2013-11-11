51962c93f75f1f4a9f555b68fbab8d25b3621d53
hadley <h.wickham@gmail.com>
2013-11-11 15:24:00 +0000

    Remove out of date usage function

--- R/utils.R
+++ R/utils.R
@@ -2,38 +2,12 @@
   if (!is.null(a)) a else b
 }
 
-# Given argument list, produce usage string for it.
-#
-# Adapted from \code{\link{prompt}}.
-#
-# @param f function, or name of function, as string
-# @return a string
-usage <- function(args) {
-  is.missing.arg <- function(arg) {
-    is.symbol(arg) && deparse(arg) == ""
-  }
-  arg_to_text <- function(arg) {
-    if (is.missing.arg(arg)) return("")
-    text <- deparse(arg, backtick = TRUE, width.cutoff = 500L)
-    text <- str_replace_all(text, "([%\\])", "\\\\\\1")
-    text <- str_replace_all(text, fixed(" "), "\u{A0}")
-    Encoding(text) <- "UTF-8"
-
-    str_c("\u{A0}=\u{A0}", paste(text, collapse = "\n"))
-  }
-
-  arg_values <- vapply(args, arg_to_text, character(1))
-
-  paste(names(args), arg_values, collapse = ", ", sep = "")
-}
-
 # Does the string contain no matter, but very well [:space:]?
 # @param string the string to check
 # @return TRUE if the string contains words, otherwise FALSE
 is.null.string <- function(string) {
   str_length(str_trim(string)) == 0
 }
-
 
 subs <- matrix(ncol = 2, byrow = T, c(
   '[]', 'sub',

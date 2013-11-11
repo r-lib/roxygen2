b68c7dbf5d1e08cea259ac27df59c41dfdb8763b
hadley <h.wickham@gmail.com>
2013-11-11 12:42:52 +0000

    Minor reorg to make profiling easier

--- R/parse.R
+++ R/parse.R
@@ -1,27 +1,3 @@
-#' Parse a source file containing roxygen directives.
-#'
-#' @param file string naming file to be parsed
-#' @return List containing parsed directives
-#' @keywords internal
-#' @export
-parse.file <- function(file, env, env_hash) {
-  srcfile <- srcfile(file)
-  
-  parse_cache$compute(c(env_hash, readLines(file, warn = FALSE)), {
-    src_refs <- attributes(parse(srcfile$filename, srcfile = srcfile))$srcref
-    pre_refs <- prerefs(srcfile, src_refs)
-
-    if (length(src_refs) == 0) return(list())
-
-    src_parsed <- lapply(src_refs, parse.srcref, env = env)
-    pre_parsed <- lapply(pre_refs, parse.preref)
-
-    stopifnot(length(src_parsed) == length(pre_parsed))
-
-    mapply(c, src_parsed, pre_parsed, SIMPLIFY = FALSE)    
-  })
-}
-
 #' Parse many files at once.
 #'
 #' @param \dots files to be parsed
@@ -36,13 +12,40 @@ parse.files <- function(paths) {
   env <- new.env(parent = parent.env(globalenv()))
   env_hash <- suppressWarnings(digest(env))
   
-  setPackageName("roxygen_destest", env)
+  setPackageName("roxygen_devtest", env)
   lapply(paths, sys.source, chdir = TRUE, envir = env)
   
-  unlist(lapply(paths, parse.file, env = env, env_hash = env_hash), 
-    recursive = FALSE)
+  parsed <- lapply(paths, parse.file, env = env, env_hash = env_hash)
+  unlist(parsed, recursive = FALSE)
 }
+
+#' Parse a source file containing roxygen directives.
+#'
+#' @param file string naming file to be parsed
+#' @return List containing parsed directives
+#' @keywords internal
+#' @export
+parse.file <- function(file, env, env_hash) {
+  srcfile <- srcfile(file)
   
+  lines <- readLines(file, warn = FALSE)
+  hash <- c(env_hash, lines)
+  
+  parse_cache$compute(hash, {
+    src_refs <- attributes(parse(srcfile$filename, srcfile = srcfile))$srcref
+    pre_refs <- prerefs(srcfile, src_refs)
+
+    if (length(src_refs) == 0) return(list())
+
+    src_parsed <- lapply(src_refs, parse.srcref, env = env)
+    pre_parsed <- lapply(pre_refs, parse.preref)
+
+    stopifnot(length(src_parsed) == length(pre_parsed))
+
+    mapply(c, src_parsed, pre_parsed, SIMPLIFY = FALSE)    
+  })
+}
+
 #' Text-parsing hack using tempfiles for more facility.
 #'
 #' @param text stringr containing text to be parsed

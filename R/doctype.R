32b8961a7d17c8ec7185c6dd5bb33479c9e89bb7
hadley <h.wickham@gmail.com>
2013-11-11 10:18:57 +0000

    Add default doctypes of S4 classes and methods

--- R/doctype.R
+++ R/doctype.R
@@ -5,3 +5,9 @@ default_doctype.data <- function(x) "data"
 
 #' @export
 default_doctype.default <- function(x) NULL
+
+#' @export
+default_doctype.s4class <- function(x) "class"
+
+#' @export
+default_doctype.s4method <- function(x) "methods"

8e757dbc20988f0875c6c0a57f7061ddca15ca07
hadley <h.wickham@gmail.com>
2013-11-11 10:13:04 +0000

    Update topic_name function in tests

--- inst/tests/test-topic-name.R
+++ inst/tests/test-topic-name.R
@@ -4,12 +4,12 @@ test_that("refclass topicname has ref-class prefix", {
   setRefClass("X1")
   on.exit(removeClass("X1"))
   obj <- object("rcclass", "X1", getRefClass("X1"))
-  expect_equal(topic_name(obj), "X1-ref-class")
+  expect_equal(default_topic_name(obj), "X1-ref-class")
 })
 
 test_that("class topicname has class prefix", {
   setClass("Y1")
   on.exit(removeClass("Y1"))
   obj <- object("s4class", "Y1", getClass("Y1"))
-  expect_equal(topic_name(obj), "Y1-class")
+  expect_equal(default_topic_name(obj), "Y1-class")
 })

550000bd08e1379cc5c86ef45b393b757afd5bed
hadley <h.wickham@gmail.com>
2013-11-15 09:09:48 -0600

    Move param tests to own file

---
+++ inst/tests/test-params.R
@@ -0,0 +1,50 @@
+context("Params")
+
+test_that("@param documents arguments", {
+  out <- roc_proc_text(roc, "
+    #' @param a an incipit letter
+    #' @param z a terminal letter
+    a <- function(a=1, z=2) {}")[[1]]
+  
+  args <- get_tag(out, "arguments")$values
+  expect_equivalent(args["a"], "an incipit letter")
+  expect_equivalent(args["z"], "a terminal letter")
+})
+
+test_that("multiple @inheritParam tags gathers all params", {
+  out <- roc_proc_text(roc, "
+    #' A.
+    #'
+    #' @param x X
+    a <- function(x) {}
+    
+    
+    #' B
+    #'
+    #' @param y Y
+    b <- function(y) {}
+    
+    #' C
+    #' 
+    #' @inheritParams a
+    #' @inheritParams b
+    c <- function(x, y) {}
+    ")
+
+  params <- get_tag(out[["c.Rd"]], "arguments")$values
+  expect_equal(length(params), 2)
+
+  expect_equal(params[["x"]], "X")
+  expect_equal(params[["y"]], "Y")
+})
+
+test_that("multiple @inheritParam inherits from existing topics", {
+  out <- roc_proc_text(roc, "
+    #' My mean
+    #'
+    #' @inheritParams base::mean
+    mymean <- function(x, trim) {}")[[1]]
+  params <- get_tag(out, "arguments")$values
+  expect_equal(length(params), 2)
+  expect_equal(sort(names(params)), c("trim", "x"))
+})

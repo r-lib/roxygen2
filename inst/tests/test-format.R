3b13c382ff12665e4069b46c713b452f17fa55a7
hadley <h.wickham@gmail.com>
2013-11-27 07:54:23 -0600

    Better test for escaping format

--- inst/tests/test-format.R
+++ inst/tests/test-format.R
@@ -23,8 +23,9 @@ test_that("@format overrides defaults", {
 test_that("@format not escaped", {
   out <- roc_proc_text(roc, "
     #' Title
-    #' @format {abc}
+    #' @format %
     x <- list(a = 1, b = 2)")[[1]]
   
-  expect_equal(get_tag(out, "format")$values, "{abc}")
+  expect_equal(get_tag(out, "format")$values, "%")
+  expect_equal(format(get_tag(out, "format")), "\\format{%}\n")
 })

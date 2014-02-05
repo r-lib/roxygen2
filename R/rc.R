def8f7048badee31b70860ed14b2b4ac5ce42bf9
hadley <h.wickham@gmail.com>
2014-02-05 11:06:26 -0600

    Correct ordering of condition

--- R/rc.R
+++ R/rc.R
@@ -4,11 +4,11 @@ rc_methods <- function(obj) {
   if (is(obj, "refObjectGenerator")) {
     obj <- obj$def
   }
-  
+
   base_methods <- getRefClass("envRefClass")$methods()
   method_names <- setdiff(ls(envir = obj@refMethods), base_methods)
   methods <- mget(method_names, envir = obj@refMethods)
-  
+
   object_from_method <- function(f) {
     object("rcmethod", f@name, f)
   }
@@ -25,41 +25,41 @@ get_method <- function(obj, method_name) {
 docstring <- function(f) {
   stopifnot(is.function(f))
   if (is.primitive(f)) return(NULL)
-  
+
   b <- body(f)
-  if (!identical(b[[1]], quote(`{`)) || length(b) <= 2) return(NULL)
-  
+  if (length(b) <= 2 || !identical(b[[1]], quote(`{`))) return(NULL)
+
   first <- b[[2]]
   if (!is.character(first)) return(NULL)
-  
+
   trim_docstring(first)
 }
 
-# Implementation converted from 
+# Implementation converted from
 # http://www.python.org/dev/peps/pep-0257/#handling-docstring-indentation
 trim_docstring <- function(docstring) {
   if (docstring == "") return(NULL)
-  
+
   # Convert tabs to spaces (using four spaces for tabs)
   # and split into a vector of lines:
   lines <- strsplit(gsub("\t", "    ", docstring), "\n")[[1]]
   if (length(lines) == 1) return(strip(lines))
-  
+
   # Determine minimum indentation (first line doesn't count):
   stripped <- gsub("^ +", "", lines)
   indent <- min(nchar(lines[-1]) - nchar(stripped[-1]))
-  
+
   # Remove indentation (first line is special):
   trimmed <- c(
     strip(lines[1]),
-    substr(lines[-1], indent + 1, 1000L)  
+    substr(lines[-1], indent + 1, 1000L)
   )
-  
+
   # Return a single string:
   string <- paste0(trimmed, collapse = "\n")
-  
+
   # Strip off trailing and leading blank lines:
   gsub("^\n+|\n+$", "", string)
 }
 
-strip <- function(x) gsub("^ +| + $", "", x) 
+strip <- function(x) gsub("^ +| + $", "", x)

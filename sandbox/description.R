05f9495126b5de82ea90fc1824c38159682e1483
hadley <h.wickham@gmail.com>
2014-02-05 11:36:46 -0600

    Remove old docs (to be replaced with new vignette

--- sandbox/description.R
+++
@@ -1,9 +0,0 @@
-source('../R/roxygen.R')
-source('../R/list.R')
-source('../R/functional.R')
-source('../R/string.R')
-source('../R/description.R')
-
-parser <- make.description.parser()
-## parser$parse(parse.description.file('../DESCRIPTION'))
-description.dependencies('../DESCRIPTION')

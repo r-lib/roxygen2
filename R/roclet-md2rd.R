06de4e184a82cdb71d4fc568a79e3cb4eea97143
fabian-s <fabian.scheipl@googlemail.com>
2014-05-04 15:18:20 +0200

    add md2rd roclet to support markdown syntax in roxygen comments

---
+++ R/roclet-md2rd.R
@@ -0,0 +1,126 @@
+#' @include roclet-rd.R
+NULL
+
+#' Roclet: make Rd files from Markdown-Roxygen comments.
+#' 
+#' Supports a limited subset of Markdown syntax in Roxygen comments: \itemize{ 
+#' \item itemized lists (lines starting with a star folowed by a blank,
+#' subsequent lines for the same item starting with 4 spaces) \item enumerated
+#' lists (first item starting with 1 followed by a period, subsequent items
+#' starting with numbers+period or star)\item \code{code}-formatting with
+#' backticks (\code{`<code>`}) \item strong/bold (\code{**<bold>**}) \item
+#' italic/emphasized text (\code{*<emph>*}) \item links to other function docs 
+#' (\code{![<function>](<package>)}) \item inline math (\code{$<math>$}) \item
+#' displayed equations (\code{$$<math>$$})}. 
+#' @seealso \code{tests/test-md2rd.R} for syntax examples.
+#' 
+#' @aliases md2rd
+#' @family roclets
+#' @export
+md2rd_roclet <- function() {
+  new_roclet(list(), "md2rd")
+}
+
+md2rd <- function(md){
+  # bold:
+  # look for star star (no star, no blank) (..., no star)  (no blank, star star)
+  rd <- str_replace_all(md, 
+                        pattern = 
+                          "(\\*\\*)([^\\*[:blank:]][^\\*]+[^[:blank:]])(\\*\\*)", 
+                        replacement = "\\\\bold{\\2}")
+  
+  # italics:
+  # look for star (no star, no blank) (..., no star)  (no blank, star)
+  rd <- str_replace_all(rd, 
+                        pattern = 
+                          "(\\*)([^\\*[:blank:]][^\\*]+[^[:blank:]])(\\*)",
+                        replacement = "\\\\emph{\\2}")
+  
+  #displayed math:
+  # look for $$ (no "$")  $$:
+  rd <-  str_replace_all(rd, 
+                         pattern = "(\\$\\$)([^$\n]+)(\\$\\$)", 
+                         replacement = "\\\\deqn{\\2}")
+  
+  # inline math:
+  # look for $ (no "$")  $:
+  rd <- str_replace_all(rd,
+                        pattern = "(\\$)([^$\n]+)(\\$)", 
+                        replacement = "\\\\eqn{\\2}")
+  
+  # code:
+  # look for ` (no "`") `:
+  rd <- str_replace_all(rd,
+                        pattern = "(\\`)([^`\n]+)(\\`)", 
+                        replacement = "\\\\code{\\2}")
+  
+  
+  # enums:
+  # look for newline 1. blank(s) [...] (newline (star OR number. OR 4 blanks) ....)
+  enumpattern <- 
+    "\\n1\\.[[:blank:]]+[[:print:]]+(\\n[*([:digit:]+.)[:blank:]{4}][[:print:]]+)+"
+  while(!any(is.na(enumposition <- str_locate(rd, enumpattern)))){
+    str <- str_extract(rd, enumpattern)
+    str <- str_replace_all(str, 
+                           pattern = "\\n\\*", 
+                           replacement = "\\\n\\\\item")
+    str <- str_replace_all(str, 
+                           pattern = "\\n[0-9]+\\.", 
+                           replacement = "\\\n\\\\item")
+    str <- paste("\n\\enumerate{", str, "}")
+    str_sub(rd, enumposition[1, 1], enumposition[1, 2]) <- str
+  }
+  
+  # lists:
+  # look for newline star blank(s) [...] (newline (star OR 4 blanks) ....)
+  itempattern <- 
+    "\\n[*][[:blank:]]+[[:print:]]+(\\n[*[:blank:]{4}][[:print:]]+)+"
+  while(!any(is.na(itemposition <- str_locate(rd, itempattern)))){
+    str <- str_extract(rd, itempattern)
+    str <- str_replace_all(str, 
+                           pattern = "\\n\\*", 
+                           replacement = "\\\n\\\\item")
+    str <- paste("\n\\itemize{", str, "}")
+    str_sub(rd, itemposition[1, 1], itemposition[1, 2]) <- str
+  }
+  
+  # links
+  #look for ![ (no "]") ]( (no")") )
+  rd <- str_replace_all(rd,
+                        pattern = "\\!\\[([^]]+)\\]\\(([^)]+)\\)", 
+                        replacement = "\\\\link[\\2]{\\1}")
+  
+  rd
+}
+
+
+
+#' @export
+roc_process.md2rd <- function(roclet, parsed, base_path, options = list()) {
+  parsed$blocks <- rapply(parsed$blocks, 
+                          f = md2rd,
+                          classes = "character",
+                          how = "replace")
+  parsed$blocks <- lapply(parsed$blocks, FUN = 
+                            function(block) {
+                              if(!is.null(block$object)) {
+                                # re-instate "pseudo-function"-class so 
+                                # default_usage() dispatches to correct method.
+                                class(block$object) <- c("function", "object")
+                              }
+                              block
+                            })
+  
+  call <- match.call()
+  call[[1]] <- roc_process.had
+  call$roclet <- rd_roclet()
+  eval(call)
+}
+
+
+#' @export
+roc_output.md2rd <- roc_output.had
+
+
+#' @export
+clean.md2rd <- clean.had
---
+++ tests/testthat/test-md2rd.R
@@ -0,0 +1,100 @@
+context("md2rd")
+roc <- md2rd_roclet()
+
+test_that("`code` formatting works", {
+  out <- roc_proc_text(roc, "
+    #' Title 
+    #'
+    #' Description contains `text formatted as code`.
+    #' @name a
+    NULL")[[1]]
+  
+  expect_equal(get_tag(out, "description")$values, 
+               "Description contains \\code{text formatted as code}.")
+})
+
+test_that("**Bold** and *emph* formatting works", {
+  out <- roc_proc_text(roc, "
+    #' Title 
+    #'
+    #' Description contains **bold text**.
+    #' @details  Details contain *italics*.
+    #' 
+    #' @name a
+    NULL")[[1]]
+  
+  expect_equal(get_tag(out, "description")$values, 
+               "Description contains \\bold{bold text}.")
+  expect_equal(get_tag(out, "details")$values, 
+               "Details contain \\emph{italics}.")
+})
+
+
+test_that("Enumerated lists work.", {
+  out <- roc_proc_text(roc, "
+    #' Title 
+    #'
+    #' Description contains enumeration:
+    #' 1. First item.
+    #'    4 spaces continue first item on second line.
+    #' * 2nd item
+    #' 3. 3rd item
+    #'    4 more spaces.
+    #' Not part of enumeration.
+    #' @name a
+    NULL")[[1]]
+  
+  expect_equal(get_tag(out, "description")$values, 
+               "Description contains enumeration:\n\\enumerate{ \n\\item First item.\n   4 spaces continue first item on second line.\n\\item 2nd item\n\\item 3rd item\n   4 more spaces. }\nNot part of enumeration.")
+})
+
+
+test_that("Itemized lists work.", {
+  out <- roc_proc_text(roc, "
+    #' Title 
+    #'
+    #' Description contains enumeration:
+    #' * First item.
+    #'    4 spaces continue first item on second line.
+    #' * 2nd item
+    #' * 3rd item
+    #'    4 more spaces.
+    #' Not part of itemization.
+    #' @name a
+    NULL")[[1]]
+  
+  expect_equal(get_tag(out, "description")$values, 
+               "Description contains enumeration:\n\\itemize{ \n\\item First item.\n   4 spaces continue first item on second line.\n\\item 2nd item\n\\item 3rd item\n   4 more spaces. }\nNot part of itemization.")
+})
+
+
+test_that("Link formatting works.", {
+  out <- roc_proc_text(roc, "
+    #' Title 
+    #'
+    #' Description contains link to ![t.test](stats).
+    #' @name a
+    NULL")[[1]]
+  
+  expect_equal(get_tag(out, "description")$values, 
+               "Description contains link to \\link[stats]{t.test}.")
+})
+
+
+
+
+test_that("Math formatting works.", {
+  out <- roc_proc_text(roc, "
+    #' Title 
+    #'
+    #' Description has in-line equation $y = 10$.
+    #' 
+    #' @details Details has displayed equation $$\\log(1)=0.$$
+    #' @name a
+    NULL")[[1]]
+  
+  expect_equal(get_tag(out, "description")$values, 
+               "Description has in-line equation \\eqn{y = 10}.")
+  expect_equal(get_tag(out, "details")$values, 
+               "Details has displayed equation \\deqn{\\log(1)=0.}")
+})

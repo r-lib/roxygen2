b55a0e714258a55b68fe7047ecfdb369d571b746
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-06-24 15:44:18 +0200

    assigning result of if statement

--- R/inherit-params.R
+++ R/inherit-params.R
@@ -34,10 +34,10 @@ get_documented_params <- function(topic, only_first = FALSE) {
   documented <- names(get_tag(topic, "param")$values)
   if (length(documented) > 0) {
     documented <- strsplit(documented, ",")
-    if (only_first)
-      documented <- vapply(documented, `[[`, character(1), 1L)
+    documented <- if (only_first)
+      vapply(documented, `[[`, character(1), 1L)
     else
-      documented <- unlist(documented)
+      unlist(documented)
   }
 
   documented

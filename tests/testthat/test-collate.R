e579e82afed13ae6ca573a0f754aa5c733889407
hadley <h.wickham@gmail.com>
2014-12-09 15:15:31 -0600

    Tests for update_collate contributed by @Geoff99.
    Closes #304

--- tests/testthat/test-collate.R
+++ tests/testthat/test-collate.R
@@ -11,9 +11,27 @@ test_that("collation as expected", {
   expect_true(before("undershorts", "pants"))
   expect_true(before(c("tie", "belt"), "jacket"))
   expect_true(before(c("socks", "undershorts", "pants"), "shoes"))
-  
+
   expected <- c('shirt.R', 'undershorts.R','pants.R', 'belt.R', 'tie.R',
     'jacket.R', 'socks.R', 'shoes.R', 'watch.R')
 
   expect_equal(results, expected)
 })
+
+test_that("Collate field unchanged when no @includes", {
+  test_pkg <- temp_copy_pkg('testCollateNoIncludes')
+  on.exit(unlink(test_pkg, recursive = TRUE))
+
+  old_desc <- read.description(file.path(test_pkg, "DESCRIPTION"))
+  update_collate(test_pkg)
+  new_desc <- read.description(file.path(test_pkg, "DESCRIPTION"))
+
+  expect_equal(names(old_desc), names(new_desc))
+  expect_identical(old_desc$Collate,new_desc$Collate)
+
+})
+
+temp_copy_pkg <- function(pkg) {
+  file.copy(normalizePath(pkg), tempdir(), recursive = TRUE)
+  normalizePath(file.path(tempdir(), pkg))
+}

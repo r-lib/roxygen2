e7ca193bc3c5a5628a5bf6349894c7a7473e500d
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-12-10 21:44:24 +0100

    support explicit @backref tag

--- tests/testthat/test-srcref-comment.R
+++ tests/testthat/test-srcref-comment.R
@@ -10,3 +10,15 @@ test_that("Source reference is included as comment", {
   backref <- format(get_tag(out[[1]], "backref"))
   expect_match(backref, "^% Please edit documentation in ")
 })
+
+test_that("Explicit @backref is included as comment", {
+  out <- roc_proc_text(roc, "
+    #' @name a
+    #' @backref back/ref.file
+    #' @backref root.file
+    #' @docType package
+    NULL")
+
+  backref <- format(get_tag(out[[1]], "backref"))
+  expect_match(backref, "^% Please edit documentation in back/ref[.]file, [.]/root[.]file")
+})

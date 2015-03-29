b1b8f0d7812aaa6225d421504c4a5c485849b049
Johannes Ranke <jranke@uni-bremen.de>
2015-03-29 03:11:49 +0200

    Keep whitespace in Authors@R field
    As the structure of the Authors@R field is potentially a bit more
    complex than that of the other fields, this commmit leads to whitespace
    being preserved when reading in the Authors@R field.

--- R/description.R
+++ R/description.R
@@ -1,6 +1,6 @@
 # Parse DESCRIPTION into convenient format
 read.description <- function(file) {
-  dcf <- read.dcf(file)
+  dcf <- read.dcf(file, keep.white = "Authors@R")
 
   dcf_list <- setNames(as.list(dcf[1, ]), colnames(dcf))
   lapply(dcf_list, str_trim)

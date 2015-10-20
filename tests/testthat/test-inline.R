6f043e61b6ac8c72bb58db7b897679f5d4823ee7
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-10-20 23:26:30 +0200

    add test

---
+++ tests/testthat/test-inline.R
@@ -0,0 +1,34 @@
+context("Inline")
+roc <- rd_roclet()
+
+test_that("Inline comments are supported", {
+  out <- roc_proc_text(roc, "
+    #' Description
+    a <- function(x) {
+      #' @param x an integer
+      stopifnot(is.integer(x))
+    }")[[1]]
+  expect_equal(get_tag(out, "param")$values, c(x="an integer"))
+})
+
+test_that("Inline comments just before the closing brace are allowed", {
+  out <- roc_proc_text(roc, "
+    #' Description
+    a <- function(x) {
+      #' @param x an integer
+      stopifnot(is.integer(x))
+
+      #' @seealso somewhere
+    }")[[1]]
+  expect_equal(get_tag(out, "seealso")$value, "somewhere")
+})
+
+test_that("Inline comments do not extend past the closing brace", {
+  out <- roc_proc_text(roc, "
+    #' Description
+    a <- function(x) {
+      #' @param x an integer
+      stopifnot(is.integer(x))
+    }; #' @seealso somewhere")[[1]]
+  expect_null(get_tag(out, "seealso"))
+})

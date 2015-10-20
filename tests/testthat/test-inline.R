394dcd829b55704006605822c934db70277cec1f
hadley <h.wickham@gmail.com>
2015-10-20 16:57:59 -0500

    Code style tweaks.
    FWIW @krlmlr

--- tests/testthat/test-inline.R
+++ tests/testthat/test-inline.R
@@ -1,18 +1,17 @@
 context("Inline")
-roc <- rd_roclet()
 
 test_that("Inline comments are supported", {
-  out <- roc_proc_text(roc, "
+  out <- roc_proc_text(rd_roclet(), "
     #' Description
     a <- function(x) {
       #' @param x an integer
       stopifnot(is.integer(x))
     }")[[1]]
-  expect_equal(get_tag(out, "param")$values, c(x="an integer"))
+  expect_equal(get_tag(out, "param")$values, c(x = "an integer"))
 })
 
 test_that("Inline comments just before the closing brace are allowed", {
-  out <- roc_proc_text(roc, "
+  out <- roc_proc_text(rd_roclet(), "
     #' Description
     a <- function(x) {
       #' @param x an integer
@@ -24,7 +23,7 @@ test_that("Inline comments just before the closing brace are allowed", {
 })
 
 test_that("Inline comments do not extend past the closing brace", {
-  out <- roc_proc_text(roc, "
+  out <- roc_proc_text(rd_roclet(), "
     #' Description
     a <- function(x) {
       #' @param x an integer

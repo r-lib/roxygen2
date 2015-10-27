71094ddb6d2209c08319228221e197ed59ddb7af
hadley <h.wickham@gmail.com>
2015-10-27 16:51:48 -0500

    Fix reexport tests

--- tests/testthat/test-reexport.R
+++ tests/testthat/test-reexport.R
@@ -5,17 +5,7 @@ test_that("exporting a call to :: produces re-exports documentation", {
     #' @export
     testthat::auto_test")[[1]]
 
-  expect_equal(get_tag(out, "title")$value, "Re-exported functions")
-  expect_equal(get_tag(out, "reexport")$value, list(pkg = "testthat", fun = "auto_test"))
-  expect_equal(get_tag(out, "keyword")$value, "internal")
-})
-
-test_that("can combine multiple rexports", {
-  out <- roc_proc_text(rd_roclet(), "
-    #' @export
-    testthat::auto_test")[[1]]
-
-  expect_equal(get_tag(out, "title")$value, "Re-exported functions")
+  expect_equal(get_tag(out, "title")$value, "Objects exported from other packages")
   expect_equal(get_tag(out, "reexport")$value, list(pkg = "testthat", fun = "auto_test"))
   expect_equal(get_tag(out, "keyword")$value, "internal")
 })

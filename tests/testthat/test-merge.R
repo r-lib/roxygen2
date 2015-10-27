15b035b60256ad712bc8166a7a7616b3f90b1586
hadley <h.wickham@gmail.com>
2015-10-27 14:21:48 -0500

    More carefully trim whitespace.
    Closes #411. Closes #414. Fixes #413.

--- tests/testthat/test-merge.R
+++ tests/testthat/test-merge.R
@@ -37,7 +37,7 @@ test_that("@section-s with identical titles are merged", {
 
   expect_equal(
     get_tag(out, "section")$values,
-    list(structure(list(name = "Haz dox", content = " Here.\n\n\n\n  Got news.")),
+    list(structure(list(name = "Haz dox", content = " Here.\n\n\n  Got news.")),
          structure(list(name = "TL", content = " DR.")),
          structure(list(name = "RT", content = " FM."))))
 })
@@ -63,7 +63,7 @@ test_that("@section-s with different titles are kept as they are", {
 
   expect_equal(
     get_tag(out, "section")$values,
-    list(structure(list(name = "Haz dox", content = " Here.\n")),
+    list(structure(list(name = "Haz dox", content = " Here.")),
          structure(list(name = "TL", content = " DR.")),
          structure(list(name = "OBTW", content = "\n  Got news.")),
          structure(list(name = "MKAY", content = " kthxbye"))))

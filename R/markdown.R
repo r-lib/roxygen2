73e5b7b039d3692908ec0c2147fe138b70584dd4
Gabor Csardi <csardi.gabor@gmail.com>
2015-11-07 20:51:26 +0000

    Support Markdown style links to functions, etc.

--- R/markdown.R
+++ R/markdown.R
@@ -133,14 +133,7 @@ markdown_tags <- list(
   },
 
   code = function(xml) {
-    ## Inline code. Check if it is a link.
-    link <- parse_manual_link(xml)
-    if (!is.null(link)) {
-      if (link$pkg != "") link$pkg <- paste0("[", link$pkg, "]")
-      paste0("\\code{\\link", link$pkg, "{", link$func, "}}")
-    } else {
-      list("\\code{", xml_contents(xml), "}")
-    }
+    list("\\code{", xml_contents(xml), "}")
   },
 
   inline_html = function(xml) {
@@ -159,9 +152,15 @@ markdown_tags <- list(
   },
 
   link = function(xml) {
-    ## Hyperlink
+    ## Hyperlink, this can also be a link to a function
     dest <- xml_attr(xml, "destination")
-    if (dest == "") {
+    contents <- xml_contents(xml)
+
+    link <- parse_link(dest, contents)
+
+    if (!is.null(link)) {
+      link
+    } else if (dest == "") {
       list("\\url{", xml_contents(xml), "}")
     } else {
       list("\\href{", dest, "}{", xml_contents(xml), "}")
@@ -180,37 +179,6 @@ markdown_tags_restricted <- markdown_tags
 
 ws_to_empty <- function(x) {
   sub("^\\s*$", "", x)
-}
-
-parse_manual_link <- function(xml) {
-  txt <- xml_text(xml)
-
-  ## package name might be missing. If not missing it must start with a
-  ## letter and must be at least two characters long
-  reg <- regexpr(
-    "^((?<p>[a-zA-Z][a-zA-Z0-9\\.]+)|)::(?<f>[a-zA-Z\\.][a-zA-Z\\.0-9]*)$",
-    txt,
-    perl = TRUE
-  )
-  if (reg != 1L) return(NULL)
-
-  pkg <- if (attr(reg, "capture.start")[,"p"] == 0) {
-    ""
-  } else {
-    substring(
-      txt,
-      attr(reg, "capture.start")[,"p"],
-      attr(reg, "capture.start")[,"p"] + attr(reg, "capture.length")[,"p"] - 1
-    )
-  }
-
-  func <- substring(
-    txt,
-    attr(reg, "capture.start")[,"f"],
-    attr(reg, "capture.start")[,"f"] + attr(reg, "capture.length")[,"f"] - 1
-  )
-
-  list(pkg = pkg, func = func)
 }
 
 ## If the string contains \preformatted, then
@@ -244,4 +212,68 @@ unescape_leading_whitespace <- function(text) {
     )
   }
   text
+}
+
+## Parse a MarkDown link, to see if we should create an Rd link
+##
+## * [](::function) becomes \code{\link{function}}
+## * [](pkg::function) becomes \code{\link[pkg]{function}}
+## * [name](::=dest) becomes \link[=dest]{name}
+## * [name](pkg::bar) becomes \link[pkg:bar]{name}
+##
+## S3/S4 classes are also supported
+## * [terms](::=terms.object) becomes \link[=terms.object]{terms}
+## * [abc](::=abc-class) becomes \link[=abc-class]{abc}
+##
+## These actually do not require any special treatment from the
+## parser, they are included in the four cases above.
+
+parse_link <- function(destination, contents) {
+
+  ## destination must contain :: exactly once, otherwise it is not an Rd link
+  if (!contains_once(destination, "::", fixed = TRUE)) return(NULL)
+
+  ## if contents is a `code tag`, then we need to move this outside
+  ## of the link, Rd does not like \link{\code{}}, only \code{\link{}}
+  pre <- post <- ""
+  if (length(contents) == 3 &&
+      xml_name(contents[[1]]) == "text" &&
+      xml_name(contents[[2]]) == "code" &&
+      xml_name(contents[[3]]) == "text") {
+    pre <- paste0(ws_to_empty(xml_text(contents[[1]])), "\\code{")
+    post <- paste0("}", ws_to_empty(xml_text(contents[[3]])))
+    contents <- xml_contents(contents[[2]])
+  }
+
+  split_contents <- strsplit(destination, "::", fixed = TRUE)[[1]]
+  pkg <- split_contents[1]
+  func <- split_contents[2]
+
+  if (is_empty_xml(contents) && pkg == "") {
+    ## [](::function) -> \code{\link{function}}
+    paste0("\\code{\\link{", func, "}}")
+
+  } else if (is_empty_xml(contents)) {
+    ## [](pkg::function) -> \code{\link[pkg]{function}}
+    paste0("\\code{\\link[", pkg, "]{", func, "}}")
+
+  } else if (pkg == "") {
+    ## [name](::=dest) -> \link[=dest]{name}
+    list(pre, paste0("\\link[", func, "]{"), contents, "}", post)
+
+  } else {
+    ## [name](pkg::bar) -> \link[pkg:bar]{name}
+    list(pre, paste0("\\link[", pkg, ":", func, "]{"), contents, "}", post)
+  }
+}
+
+## Check if a string contains a pattern exactly once. Overlapping
+## patterns do not count.
+
+contains_once <- function(x, pattern, ...) {
+  length(strsplit(x = x, split = pattern, ...)[[1]]) == 2
+}
+
+is_empty_xml <- function(x) {
+  is(x, "xml_nodeset") && length(x) == 0
 }

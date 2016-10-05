d27d80753b546aacbcedbf7abcaaf16f45dafefb
hb <hb@aroma-project.org>
2016-10-04 17:45:54 -0700

    Support setMethodS3() and setConstructorS3() of R.methodsS3 and R.oo

--- R/object-from-call.R
+++ R/object-from-call.R
@@ -136,3 +136,22 @@ parser_setReplaceMethod <- function(call, env, block) {
 
   object(list(pkg = pkg, fun = fun), alias = fun, type = "import")
 }
+
+parser_setMethodS3 <- function(call, env, block) {
+  # R.methodsS3::setMethodS3(name, class, ...)
+  method <- as.character(call[[2]])
+  class <- as.character(call[[3]])
+  name <- paste(method, class, sep=".")
+  value <- get(name, env)
+  value <- standardise_obj(name, value, env, block)
+  object(value, name)
+
+}
+
+parser_setConstructorS3 <- function(call, env, block) {
+  # R.oo::setConstructorS3(name, ...)
+  name <- as.character(call[[2]])
+  value <- get(name, env)
+  value <- standardise_obj(name, value, env, block)
+  object(value, name)
+}

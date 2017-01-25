7a601b4de06ffcf500b47df6ecf7a5c9c4caa6fd
Kirill Müller <krlmlr+r@mailbox.org>
2017-01-25 14:02:32 +0100

    refine test

--- tests/testthat/test-nonASCII.R
+++ tests/testthat/test-nonASCII.R
@@ -7,13 +7,13 @@ test_that("can generate nonASCII document", {
   expect_output(roxygenise(test_pkg, roclets = "rd"), "printChineseMsg[.]Rd")
   expect_true(file.exists(file.path(test_pkg, "man", "printChineseMsg.Rd")))
 
-  con <- file(file.path(test_pkg, "man", "printChineseMsg.Rd"), encoding = "UTF-8")
-  on.exit(close(con), add = TRUE)
-
-  cnChar <- readLines(con)
+  cnChar <- readLines(file.path(test_pkg, "man", "printChineseMsg.Rd"))
 
   # Because the parse in testthat::test don't specify encoding to UTF-8 as well,
   # so we have to use unicode escapes.
   expect_true(any(grepl("\u6211\u7231\u4e2d\u6587", cnChar)))
   expect_true(any(grepl("\u4e2d\u6587\u6ce8\u91ca", cnChar)))
+
+  # No output on second run
+  expect_output(roxygenise(test_pkg, roclets = "rd"), NA)
 })

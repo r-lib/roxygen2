25473537887cd9757ead1c9f33442647571ac482
hadley <h.wickham@gmail.com>
2017-08-17 17:02:51 -0500

    Vigentte tweaks
    * Remove source from github
    * Build them during check (to avoid spurious warnings)

--- vignettes/collate.R
+++
@@ -1,7 +0,0 @@
-## ---- include = FALSE----------------------------------------------------
-knitr::opts_chunk$set(comment = "#>", collapse = TRUE)
-
-## ---- eval = FALSE-------------------------------------------------------
-#  #' @include class-a.r
-#  setClass("B", contains = "A")
-

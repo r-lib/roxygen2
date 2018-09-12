90393ecf8436cc8043877ba77e2fa3001456cad9
brodieG <brodieG@users.noreply.github.com>
2018-09-11 20:24:15 -0400

    run through styler::style_file

--- R/source.R
+++ R/source.R
@@ -1,9 +1,9 @@
 package_files <- function(path) {
   all <- normalizePath(r_files(path))
 
-  collate <- desc::desc_get_collate(file=file.path(path, 'DESCRIPTION'))
+  collate <- desc::desc_get_collate(file = file.path(path, "DESCRIPTION"))
 
-  collate <- normalizePath(file.path(path, 'R', collate))
+  collate <- normalizePath(file.path(path, "R", collate))
 
   rfiles <- c(collate, setdiff(all, collate))
   ignore_files(rfiles, path)

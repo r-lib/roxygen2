9a64bd0ed40ef7d5efbbb93b20e2cd5b7b7bc5d2
Hadley Wickham <h.wickham@gmail.com>
2019-08-23 15:33:18 -0500

    Tidy up tests for object_format()

--- tests/testthat/test-rd.R
+++ tests/testthat/test-rd.R
@@ -95,3 +95,40 @@ test_that("documenting NA gives useful error message (#194)", {
     "Missing name"
   )
 })
+
+
+# format ------------------------------------------------------------------
+
+test_that("@format overrides defaults", {
+  out <- roc_proc_text(rd_roclet(), "
+    #' Title
+    #'
+    #' @format abc
+    #'
+    x <- list(a = 1, b = 2)")[[1]]
+
+  expect_equal(get_tag(out, "format")$values, "abc")
+})
+
+test_that("@format NULL suppresses default usage", {
+  out <- roc_proc_text(rd_roclet(), "
+    #' Title
+    #'
+    #' @format NULL
+    #'
+    x <- list(a = 1, b = 2)")[[1]]
+
+  expect_equal(get_tag(out, "format")$values, NULL)
+})
+
+test_that("@format not escaped", {
+  out <- roc_proc_text(rd_roclet(), "
+    #' Title
+    #' @format %
+    #'
+    x <- list(a = 1, b = 2)")[[1]]
+
+  expect_equal(get_tag(out, "format")$values, "%")
+  expect_equal(format(get_tag(out, "format")), "\\format{%}")
+})
+

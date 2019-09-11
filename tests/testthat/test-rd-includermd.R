afaa3af65d04b6798621076fdb50be188a0413aa
Gábor Csárdi <csardi.gabor@gmail.com>
2019-09-11 10:59:31 +0100

    More includeRmd tests

--- tests/testthat/test-rd-includermd.R
+++ tests/testthat/test-rd-includermd.R
@@ -61,3 +61,69 @@ test_that("markdown with headers", {
   expect_equal_strings(out1$fields$details$values, exp_details)
   expect_equal_strings(out1$fields$rawRd$values, exp_secs)
 })
+
+test_that("subsection within details", {
+  tmp <- tempfile(fileext = ".md")
+  on.exit(unlink(tmp, recursive = TRUE), add = TRUE)
+  cat(sep = "\n", file = tmp,
+    "Text at the front",
+    "",
+    "",
+    "## Subsection in details",
+    "",
+    "Some subsection text")
+  rox <- sprintf("
+    #' Title
+    #' @includeRmd %s
+    #' @name foobar
+    NULL", tmp)
+  out1 <- roc_proc_text(rd_roclet(), rox)[[1]]
+  exp_details <- paste0(
+    "Text at the front",
+    "\\subsection{Subsection in details}{ Some subsection text }"
+  )
+  expect_equal_strings(out1$fields$details$values, exp_details)
+})
+
+test_that("links to functions", {
+  tmp <- tempfile(fileext = ".md")
+  on.exit(unlink(tmp, recursive = TRUE), add = TRUE)
+  cat(sep = "\n", file = tmp,
+    "This is a link: [roxygenize()].",
+    "Another one: [stringr::str_length()]"
+  )
+  rox <- sprintf("
+    #' Title
+    #' @includeRmd %s
+    #' @name foobar
+    NULL", tmp)
+  out1 <- roc_proc_text(rd_roclet(), rox)[[1]]
+  exp_details <- paste0(
+    "This is a link: \\code{\\link[=roxygenize]{roxygenize()}}.",
+    "Another one:\n\\code{\\link[stringr:str_length]{stringr::str_length()}}"
+  )
+  expect_equal_strings(out1$fields$details$values, exp_details)
+})
+
+test_that("links to functions, with anchors", {
+  tmp <- tempfile(fileext = ".md")
+  on.exit(unlink(tmp, recursive = TRUE), add = TRUE)
+  cat(sep = "\n", file = tmp,
+    "This is a link: [roxygenize()].",
+    "Another one: [stringr::str_length()]",
+    "",
+    "[roxygenize()]: https://roxygen2.r-lib.org/reference/roxygenize.html",
+    "[stringr::str_length()]: https://stringr.tidyverse.org/reference/str_length.html"
+  )
+  rox <- sprintf("
+    #' Title
+    #' @includeRmd %s
+    #' @name foobar
+    NULL", tmp)
+  out1 <- roc_proc_text(rd_roclet(), rox)[[1]]
+  exp_details <- paste0(
+    "This is a link: \\code{\\link[=roxygenize]{roxygenize()}}.",
+    "Another one:\n\\code{\\link[stringr:str_length]{stringr::str_length()}}"
+  )
+  expect_equal_strings(out1$fields$details$values, exp_details)
+})

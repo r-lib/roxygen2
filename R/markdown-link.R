1d2a8623c504f1a048dd71edb1a1471bcf1c2252
Hadley Wickham <h.wickham@gmail.com>
2019-09-12 14:45:16 -0500

    Clarify return type of get_md_linkrefs()

--- R/markdown-link.R
+++ R/markdown-link.R
@@ -52,20 +52,22 @@ get_md_linkrefs <- function(text) {
     )
   )[[1]]
 
-  if (length(refs) == 0) return("")
+  if (length(refs) == 0) {
+    return(character())
+  }
 
   ## For the [fun] form the link text is the same as the destination.
   # Need to check both NA and "" for different versions of stringr
   refs[, 3] <- ifelse(is.na(refs[,3]) | refs[,3] == "", refs[, 2], refs[,3])
 
   refs3encoded <- map_chr(refs[,3], URLencode)
-  ref_lines <- paste0("[", refs[, 3], "]: ", "R:", refs3encoded)
-  ref_lines
+  paste0("[", refs[, 3], "]: ", "R:", refs3encoded)
 }
 
 add_linkrefs_to_md <- function(text) {
   ref_lines <- get_md_linkrefs(text)
-  if (! length(ref_lines)) return(text)
+  if (length(ref_lines) == 0)
+    return(text)
   ref_text <- paste0(ref_lines, collapse = "\n")
   paste0(text, "\n\n", ref_text, "\n")
 }

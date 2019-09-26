70778a0ce7c7577b656772b72f3dac313393256e
Gábor Csárdi <csardi.gabor@gmail.com>
2019-09-26 09:21:58 +0100

    Make block_to_rd S3 generic
    This will let us define a custom page formatter
    for R6 classes.

--- R/rd.R
+++ R/rd.R
@@ -100,6 +100,18 @@ needs_doc <- function(block) {
 # Tag processing functions ------------------------------------------------
 
 block_to_rd <- function(block, base_path, env) {
+  UseMethod("block_to_rd")
+}
+
+#' @export
+
+block_to_rd.default <- function(block, ...) {
+  stop("Internal roxygen error, unknown block type")
+}
+
+#' @export
+
+block_to_rd.roxy_block <- function(block, base_path, env) {
   # Must start by processing templates
   block <- process_templates(block, base_path)
 

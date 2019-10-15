5411dbc366e08eb6c15ba0cf8e73668fa8ff94a6
Gabor Csardi <csardi.gabor@gmail.com>
2019-10-15 01:29:02 -0700

    Add test case for Unicode file names on Win

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -10,4 +10,16 @@ test_that("nice_name protects against invalid characters", {
   expect_equal(nice_name("[.a"), "sub-.a")
 })
 
-
+test_that("same_contents with Unicode file names on Windows", {
+  skip_on_cran()
+  if (.Platform$OS.type != "windows") skip("Only on windows")
+  tmp <- tempfile()
+  on.exit(unlink(tmp, recursive = TRUE), add = TRUE)
+  dir.create(tmp)
+  path <- file.path(tmp, "tricky-\u0151")
+  contents <- "foobar"
+  # Need to write raw, otherwise it will be \r\n on Windows
+  writeBin(charToRaw(paste0(contents, "\n")), con = path)
+  expect_silent(res <- same_contents(path, contents))
+  expect_true(res)
+})

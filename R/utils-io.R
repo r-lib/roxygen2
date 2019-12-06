a3fd62d38fbd22902aa37f2a9da954215cb39f7d
Jim Hester <james.f.hester@gmail.com>
2019-12-06 14:51:49 -0500

    Convert embedded newlines as well

--- R/utils-io.R
+++ R/utils-io.R
@@ -12,6 +12,9 @@ write_lines <- function(text, path) {
     line_ending <- "\n"
   }
 
+  # we need to convert any embedded newlines as well
+  text <- gsub("\r?\n", line_ending, text)
+
   path <- file(path, open = "wb")
   base::writeLines(enc2utf8(text), path, sep = line_ending, useBytes = TRUE)
   close(path)

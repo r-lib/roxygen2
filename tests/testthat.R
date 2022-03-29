f6be0604de69a08aed6b488bc77aac95399ff7ee
Hadley Wickham <h.wickham@gmail.com>
2022-03-29 15:08:43 -0500

    Update description + testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,8 +1,4 @@
 library(testthat)
 library(roxygen2)
 
-if (requireNamespace("xml2")) {
-  test_check("roxygen2", reporter = MultiReporter$new(reporters = list(JunitReporter$new(file = "test-results.xml"), CheckReporter$new())))
-} else {
-  test_check("roxygen2")
-}
+test_check("roxygen2")

3a93c571b8e802600960911bd83c8e6c2bc07a3b
Hadley Wickham <h.wickham@gmail.com>
2022-04-11 10:20:53 -0500

    Set Encoding in DESCRIPTION

--- tests/testthat/test-roxygenize-setup.R
+++ tests/testthat/test-roxygenize-setup.R
@@ -6,7 +6,6 @@ test_that("useful error if no DESCRIPTION", {
 
 test_that("informs about initial setup", {
   local_package_copy(test_path("empty"))
-  desc::desc_set(Encoding = "UTF-8")
 
   expect_snapshot(roxygen_setup(cur_version = "8.0.0"))
 })
@@ -20,14 +19,14 @@ test_that("warns about non UTF-8 encoding", {
 
 test_that("warns if roxygen version is too new", {
   local_package_copy(test_path("empty"))
-  desc::desc_set(Encoding = "UTF-8", RoxygenNote = "10.0.0")
+  desc::desc_set(RoxygenNote = "10.0.0")
 
   expect_snapshot(roxygen_setup(cur_version = "8.0.0"))
 })
 
 test_that("informs about major changes in 7.0.0", {
   local_package_copy(test_path("empty"))
-  desc::desc_set(Encoding = "UTF-8", RoxygenNote = "5.0.0")
+  desc::desc_set(RoxygenNote = "5.0.0")
 
   expect_snapshot(roxygen_setup(cur_version = "8.0.0"))
 })

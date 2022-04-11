ba655d47d7ab0acfd8da1c52f082a1c9450a3d2f
Hadley Wickham <h.wickham@gmail.com>
2022-04-11 10:10:43 -0500

    Convert primary user feedback to cli (#1329)
    * Polish `local_package_copy()` and replace `temp_copy_pkg()`
    * Refactor `roxygen_setup()` and move all related code to its own file
    * `first_time()` now short circuits if RoxygenNote set

---
+++ tests/testthat/test-roxygenize-setup.R
@@ -0,0 +1,33 @@
+test_that("useful error if no DESCRIPTION", {
+  local_package_copy(test_path("no-desc"))
+
+  expect_snapshot(roxygen_setup(), error = TRUE)
+})
+
+test_that("informs about initial setup", {
+  local_package_copy(test_path("empty"))
+  desc::desc_set(Encoding = "UTF-8")
+
+  expect_snapshot(roxygen_setup(cur_version = "8.0.0"))
+})
+
+test_that("warns about non UTF-8 encoding", {
+  local_package_copy(test_path("empty"))
+  desc::desc_set(Encoding = "latin1", RoxygenNote = "8.0.0")
+
+  expect_snapshot(roxygen_setup(cur_version = "8.0.0"))
+})
+
+test_that("warns if roxygen version is too new", {
+  local_package_copy(test_path("empty"))
+  desc::desc_set(Encoding = "UTF-8", RoxygenNote = "10.0.0")
+
+  expect_snapshot(roxygen_setup(cur_version = "8.0.0"))
+})
+
+test_that("informs about major changes in 7.0.0", {
+  local_package_copy(test_path("empty"))
+  desc::desc_set(Encoding = "UTF-8", RoxygenNote = "5.0.0")
+
+  expect_snapshot(roxygen_setup(cur_version = "8.0.0"))
+})

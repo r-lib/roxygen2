763ce14bc79d59708af74b86564c2b070323aac2
Michael Chirico <chiricom@google.com>
2024-01-15 08:15:35 -0800

    bad grammar

--- R/namespace.R
+++ R/namespace.R
@@ -271,7 +271,7 @@ roxy_tag_ns.roxy_tag_importFrom <- function(x, block, env) {
     # be sure to match '%>%', `%>%`, "%>%" all to %>% given by getNamespaceExports, #1570
     unknown_idx <- !normalize_quotes(importing) %in% getNamespaceExports(pkg)
     if (any(unknown_idx)) {
-      warn_roxy_tag(x, "Excluding unknown {cli::qty(sum(unknown_idx))} export{?s} in from {.package {pkg}}: {.code {importing[unknown_idx]}}")
+      warn_roxy_tag(x, "Excluding unknown {cli::qty(sum(unknown_idx))} export{?s} from {.package {pkg}}: {.code {importing[unknown_idx]}}")
       if (all(unknown_idx)) {
         return(NULL)
       }

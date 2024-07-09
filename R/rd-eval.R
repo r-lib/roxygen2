0c16adc6658f81b9066d0f68b8253c7a2e966bc4
olivroy <52606734+olivroy@users.noreply.github.com>
2024-07-09 11:01:23 -0400

    style

--- R/rd-eval.R
+++ R/rd-eval.R
@@ -23,10 +23,11 @@ local_reproducible_output <- function(.envir = parent.frame()) {
     .local_envir = .envir
   )
   withr::local_envvar(RSTUDIO = NA, .local_envir = .envir)
-  if (isTRUE(capabilities("NLS")) && Sys.getenv("LANG") !=
-      "C") {
+
+  if (isTRUE(capabilities("NLS")) && Sys.getenv("LANG") != "C") {
     lang <- unname(desc::desc_get("Language")) %|% "en"
     withr::local_language(lang, .local_envir = .envir)
   }
+
   withr::local_collate("C", .local_envir = .envir)
 }

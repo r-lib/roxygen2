d702d5625e8e181066f7658c15e4814edda7333d
Maëlle Salmon <maelle.salmon@yahoo.se>
2026-03-06 14:19:14 +0100

    docs: document the namespace roclet a bit more

--- R/namespace.R
+++ R/namespace.R
@@ -1,9 +1,10 @@
 #' Roclet: make `NAMESPACE`
 #'
 #' @description
-#' This roclet automates the production of a `NAMESPACE` file, which controls
+#' This [roclet()] automates the production of a `NAMESPACE` file, which controls
 #' the functions imported and exported by your package, as described in
 #' [Writing R extensions](https://cran.r-project.org/doc/manuals/r-release/R-exts.html).
+#' It is run by default by [roxygenize()].
 #'
 #' The `NAMESPACE` is generated in two passes: the first generates only
 #' import directives (because this can be computed without evaluating package
@@ -17,15 +18,25 @@
 #' @examples
 #' # The most common namespace tag is @@export, which declares that a function
 #' # is part of the external interface of your package
-#' #' @export
-#' foofy <- function(x, y, z) {
-#' }
+#' example1 <- c(
+#'   "#' @export",
+#'   "foofy <- function(x, y, z) {",
+#'   "}"
+#' )
+#' cat(example1, sep = "\n")
+#' # This results in the following line in `NAMESPACE`:
+#' roc_proc_text(namespace_roclet(), example1)
 #'
 #' # You'll also often find global imports living in a file called
 #' # R/{package}-package.R.
-#' #' @@importFrom magrittr %>%
-#' #' @@import rlang
-#' NULL
+#' example2 <- c(
+#'   "#' @@importFrom magrittr %>%",
+#'   "#' @@import rlang",
+#'   "NULL"
+#' )
+#' cat(example2, sep = "\n")
+#' # This results in the following lines in `NAMESPACE`:
+#' roc_proc_text(namespace_roclet(), example2)
 namespace_roclet <- function() {
   roclet("namespace")
 }

dc1656fe10c212ad590e4e5dab5302e202ad915f
Hadley Wickham <h.wickham@gmail.com>
2026-03-09 07:58:37 -0500

    Polishing

--- src/isComplete.cpp
+++ src/isComplete.cpp
@@ -5,14 +5,12 @@
 // and } have special meaning in almost all parts of an Rd file. In code,
 // strings must also match, except in comments.
 
-enum class State {
-  Rd,
-  RdEscape,
-  String,
-  StringEscape,
-  RComment,
-  RdComment
-};
+enum class State { Rd, RdEscape, String, StringEscape, RComment, RdComment };
+
+bool is_complete(int braces, State state) {
+  return braces == 0 && (state == State::Rd || state == State::RComment ||
+                         state == State::RdComment);
+}
 
 // The two functions are very similar, so we use a common
 // implementation and select the functionality via the
@@ -31,6 +29,39 @@ int roxygen_parse_tag(std::string string, bool is_code = false, int mode = 0) {
     char cur = string[i];
 
     switch (state) {
+    case State::Rd:
+      switch (cur) {
+      case '{':
+        braces++;
+        break;
+      case '}':
+        braces--;
+        break;
+      case '\\':
+        state = State::RdEscape;
+        break;
+      case '#':
+        if (is_code)
+          state = State::RComment;
+        break;
+      case '%':
+        state = State::RdComment;
+        break;
+      case '\'':
+        if (is_code) {
+          state = State::String;
+          string_delim = '\'';
+        }
+        break;
+      case '"':
+        if (is_code) {
+          state = State::String;
+          string_delim = '"';
+        }
+        break;
+      }
+      break;
+
     case State::RdEscape:
       state = State::Rd;
       break;
@@ -70,63 +101,21 @@ int roxygen_parse_tag(std::string string, bool is_code = false, int mode = 0) {
         state = State::Rd;
       }
       break;
-
-    case State::Rd:
-      switch (cur) {
-      case '{':
-        braces++;
-        break;
-      case '}':
-        braces--;
-        break;
-      case '\\':
-        state = State::RdEscape;
-        break;
-      case '#':
-        if (is_code)
-          state = State::RComment;
-        break;
-      case '%':
-        state = State::RdComment;
-        break;
-      case '\'':
-        if (is_code) {
-          state = State::String;
-          string_delim = '\'';
-        }
-        break;
-      case '"':
-        if (is_code) {
-          state = State::String;
-          string_delim = '"';
-        }
-        break;
-      }
-      break;
     }
 
     if (mode == 1) {
-      bool complete =
-          braces == 0 && state != State::RdEscape && state != State::String && state != State::StringEscape;
+      bool complete = is_complete(braces, state);
       if (complete && i + 1 < n && string[i + 1] != '{') {
         return i;
       }
     }
   }
 
-  bool complete =
-      braces == 0 && state != State::RdEscape && state != State::String && state != State::StringEscape;
-
+  bool complete = is_complete(braces, state);
   if (mode == 0) {
-    if (complete)
-      return 1;
-    else
-      return 0;
+    return complete ? 1 : 0;
   } else {
-    if (complete)
-      return n - 1;
-    else
-      return -1;
+    return complete ? n - 1 : -1;
   }
 }
 

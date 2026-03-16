e47feb2b4ecd7037624060ba579ad57f81888756
Hadley Wickham <h.wickham@gmail.com>
2026-03-16 13:41:24 -0500

    Don't need brio

--- tests/testthat/test-rd-r6.R
+++ tests/testthat/test-rd-r6.R
@@ -5,7 +5,7 @@ test_that("integration test", {
 
   rd_files <- sort(dir(file.path(path, "man"), full.names = TRUE))
   for (rd_file in rd_files) {
-    expect_snapshot(cat(brio::read_lines(rd_file), sep = "\n"))
+    expect_snapshot(cat(read_lines(rd_file), sep = "\n"))
     expect_silent(chk <- tools::checkRd(rd_file))
     expect_equal(length(chk), 0L)
   }
